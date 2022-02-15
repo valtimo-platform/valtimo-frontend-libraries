aec661dc725deeb5c9c7bcaa995752ba69a5844d
mritense <maxim.buur@ritense.com>
2022-02-15 16:19:30 +0100

    fix more icons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -90,7 +90,7 @@ export const environment: ValtimoConfig = {
         roles: [ROLE_USER],
         link: ['/analysis'],
         title: 'Analysis',
-        iconClass: 'icon mdi mdi-chart',
+        iconClass: 'icon mdi mdi-chart-bar',
         sequence: 3,
       },
       {
