dc79036e644e63113b6c3a0e136aec6a530007bb
mritense <maxim.buur@ritense.com>
2022-02-28 09:23:14 +0100

    fix form management linting

--- projects/valtimo/form-management/src/lib/form-management-upload/form-management-upload.component.ts
+++ projects/valtimo/form-management/src/lib/form-management-upload/form-management-upload.component.ts
@@ -152,7 +152,7 @@ export class FormManagementUploadComponent implements AfterViewInit, OnDestroy {
 
   private stringIsValidJson(string: string) {
     try {
-      // tslint:disable-next-line
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       JSON.parse(string)?.formDefinition?.components;
     } catch (e) {
       this.clearDropzone();
