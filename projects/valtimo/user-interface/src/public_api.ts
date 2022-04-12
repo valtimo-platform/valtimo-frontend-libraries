d2b639ef013a0505d009990a1fd2e2786d014c2f
mritense <maxim.buur@ritense.com>
2022-04-12 08:14:22 +0200

    add text input component

--- projects/valtimo/user-interface/src/public_api.ts
+++ projects/valtimo/user-interface/src/public_api.ts
@@ -50,3 +50,6 @@ export * from './lib/components/card/card.module';
 // Title component
 export * from './lib/components/title/title.component';
 export * from './lib/components/title/title.module';
+// Text input
+export * from './lib/components/text-input/text-input.component';
+export * from './lib/components/text-input/text-input.module';
