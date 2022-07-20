e58615baf278adc8f3bd25a496b3ad2247e7ed7c
mritense <maxim.buur@ritense.com>
2022-07-20 16:22:08 +0200

    revert dimpact copyright change

--- projects/valtimo/open-zaak/src/lib/email-extension/email-extension.component.ts
+++ projects/valtimo/open-zaak/src/lib/email-extension/email-extension.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Ritense BV, the Netherlands.
+ * Copyright 2020 Dimpact.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
