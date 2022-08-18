72c5759ca9725f3ad61b8d8477808c6057ba0fbe
Rick <86724253+Rick-Ritense@users.noreply.github.com>
2022-08-18 16:25:36 +0200

    feature 36693 - Added extra tooltips (#171)
    * feature 36693 - Added extra tooltips

--- projects/valtimo/plugin/src/lib/plugins/open-zaak/open-zaak-plugin.specification.ts
+++ projects/valtimo/plugin/src/lib/plugins/open-zaak/open-zaak-plugin.specification.ts
@@ -28,24 +28,39 @@ const openZaakPluginSpecification: PluginSpecification = {
       description:
         'OpenZaak is een productiewaardig API platform die de API-standaard voor zaakgericht werken implementeert (de ZGW-API’s).',
       configurationTitle: 'Configuratienaam',
+      configurationTitleTooltip:
+        'Onder deze naam zal de plugin te herkennen zijn in de rest van de applicatie',
       clientId: 'Client ID',
+      clientIdTooltip:
+        'Vul hier het clientId in dat geconfigureerd staat onder OpenZaak-beheer (zie API authorisaties > Applicaties). Dit clientId moet de juiste authorisaties hebben voor de benodigde functionaliteit',
       clientSecret: 'Secret',
+      clientSecretTooltip: 'Vul de secret in die hoort bij de clientId hierboven',
     },
     en: {
       title: 'OpenZaak',
       description:
         'OpenZaak is a production-worthy API platform that implements the API standard for case-oriented working (the ZGW APIs).',
       configurationTitle: 'Configuration name',
+      configurationTitleTooltip:
+        'Under this name, the plugin will be recognizable in the rest of the application',
       clientId: 'Client ID',
+      clientIdTooltip:
+        'Enter the clientId here which is configured under OpenZaak management (see API authorizations > Applications). This clientId must have the correct authorizations for the required functionality',
       clientSecret: 'Secret',
+      clientSecretTooltip: 'Enter the secret associated with the clientId above',
     },
     de: {
       title: 'OpenZaak',
       description:
         'OpenZaak ist eine produktionstaugliche API-Plattform, die den API-Standard für fallorientiertes Arbeiten (die ZGW-APIs) implementiert.',
       configurationTitle: 'Konfigurationsname',
+      configurationTitleTooltip:
+        'Unter diesem Namen wird das Plugin im Rest der Anwendung erkennbar sein',
       clientId: 'Client ID',
+      clientIdTooltip:
+        'Geben Sie hier die clientId ein, die unter OpenZaak-Verwaltung konfiguriert ist (siehe API-Berechtigungen > Anwendungen). Diese clientId muss die richtigen Berechtigungen für die erforderliche Funktionalität haben',
       clientSecret: 'Secret',
+      clientSecretTooltip: 'Geben Sie das mit der obigen clientId verknüpfte Geheimnis ein',
     },
   },
 };
--- projects/valtimo/plugin/src/lib/plugins/smart-documents/smart-documents-plugin.specification.ts
+++ projects/valtimo/plugin/src/lib/plugins/smart-documents/smart-documents-plugin.specification.ts
@@ -31,12 +31,16 @@ const smartDocumentsPluginSpecification: PluginSpecification = {
       title: 'SmartDocuments',
       description: 'Automatiseer documenten met slimme templates.',
       configurationTitle: 'Configuratienaam',
+      configurationTitleTooltip:
+        'Onder deze naam zal de plugin te herkennen zijn in de rest van de applicatie',
       url: 'SmartDocuments URL',
       username: 'Gebruikersnaam',
       password: 'Wachtwoord',
       'generate-document': 'Document genereren',
       templateGroup: 'Template-groep',
       templateName: 'Template-naam',
+      templateDataTooltip:
+        'De rechter value-kolom ondersteunt ook het gebruik van procesvariabelen zoals pv: en doc:',
       format: 'Documentformaat',
       templateData: 'Template-data',
       resultingDocumentProcessVariableName: 'Naam procesvariabele voor opslag document',
@@ -49,6 +53,8 @@ const smartDocumentsPluginSpecification: PluginSpecification = {
       title: 'SmartDocuments',
       description: 'Automate documents with smart templates.',
       configurationTitle: 'Configuration name',
+      configurationTitleTooltip:
+        'With this name the plugin will be recognizable in the rest of the application',
       url: 'SmartDocuments URL',
       username: 'Username',
       password: 'Password',
@@ -57,6 +63,8 @@ const smartDocumentsPluginSpecification: PluginSpecification = {
       templateName: 'Template name',
       format: 'Document format',
       templateData: 'Template data',
+      templateDataTooltip:
+        'The right value-column also supports the use of process variables such as pv: and doc:',
       resultingDocumentProcessVariableName: 'Process variable name for storing document',
       resultingDocumentProcessVariableNameTooltip:
         'The location of the generated document is stored in a process variable. This process variable can be used to access the document in another BPMN task.',
@@ -67,6 +75,8 @@ const smartDocumentsPluginSpecification: PluginSpecification = {
       title: 'SmartDocuments',
       description: 'Automatisieren Sie Dokumente mit intelligenten Templates.',
       configurationTitle: 'Konfigurationsname',
+      configurationTitleTooltip:
+        'An diesem Namen wird das Plugin im Rest der Anwendung erkennbar sein',
       url: 'SmartDocuments URL',
       username: 'Nutzername',
       password: 'Passwort',
@@ -75,6 +85,8 @@ const smartDocumentsPluginSpecification: PluginSpecification = {
       templateName: 'Templatename',
       format: 'Dokumentformat',
       templateData: 'Templatedaten',
+      templateDataTooltip:
+        'Die rechte Wertespalte unterstützt auch die Verwendung von Prozessvariablen wie pv: und doc:',
       resultingDocumentProcessVariableName: 'Prozessvariablenname zum Speichern des Dokuments',
       resultingDocumentProcessVariableNameTooltip:
         'Der Ort des generierten Dokuments wird in einer Prozessvariablen gespeichert. Diese Prozessvariable wird verwendet, um in einer anderen BPMN-Aufgabe auf das Dokument zuzugreifen.',
