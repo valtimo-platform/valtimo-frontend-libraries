80764091fd07b14be102bc1de01e1fa29607c354
maisamaximo <maisa.maximo@ritense.com>
2022-08-24 15:40:46 +0100

    delete unused service file

--- projects/valtimo/components/src/lib/components/left-sidebar/services/left-sidebar.service.ts
+++
@@ -1,9 +0,0 @@
-import {Inject, Injectable} from '@angular/core';
-import {CustomTaskList, VALTIMO_CONFIG, ValtimoConfig} from '@valtimo/config';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class LeftSidebarService {
-  constructor(@Inject(VALTIMO_CONFIG) private valtimoConfig: ValtimoConfig) {}
-}
