457da137f58d210d6ab6612dbab4563c63e9e0dc
mritense <79840403+mritense@users.noreply.github.com>
2022-10-25 14:20:11 +0200

    feat: dependency upgrades major 10 (#253)
    * check until bpmn-js root package.json
    
    * remove copyfiles
    
    * upgrade testing deps
    
    * upgrade keycloak
    
    * upgrade until ngx-toastr
    
    * http translate loader upgrade
    
    * upgrade popper, remove old popperjs dep
    
    * upgrade; remove beagle resources
    
    * remove roboto fontface
    
    * protractor to cypress
    
    * remove old beagle deps
    
    * upgrade further deps
    
    * upgrade further deps
    
    * upgrade further deps
    
    * remove json server
    
    * lock formiojs

--- projects/valtimo/config/src/lib/services/config.service.ts
+++ projects/valtimo/config/src/lib/services/config.service.ts
@@ -53,10 +53,9 @@ export class ConfigService {
         endpointUri: this.formatUrlTrailingSlash(config.valtimoApi.endpointUri, true),
       },
       ...(translationResourcesConfig && {
-        translationResources: translationResourcesConfig.map(resource => ({
-          prefix: this.formatUrlTrailingSlash(resource.prefix, true),
-          suffix: resource.suffix,
-        })),
+        translationResources: translationResourcesConfig.map(resource =>
+          this.formatUrlTrailingSlash(resource, true)
+        ),
       }),
     };
   }
