f2d001ad93c60e814e2bed6d3e44d0964fd606bf
maisamaximo <maisa.maximo@ritense.com>
2022-11-23 14:11:34 +0100

    add checkbox template

--- projects/valtimo/dossier-management/src/lib/dossier-management-assignee/dossier-management-assignee.component.ts
+++ projects/valtimo/dossier-management/src/lib/dossier-management-assignee/dossier-management-assignee.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DocumentService} from '@valtimo/document';
 
 @Component({
   selector: 'valtimo-dossier-management-assignee',
   templateUrl: './dossier-management-assignee.component.html',
-  styleUrls: ['./dossier-management-assignee.component.css']
+  styleUrls: ['./dossier-management-assignee.component.css'],
 })
 export class DossierManagementAssigneeComponent implements OnInit {
+  canHaveAssignee = false;
+  constructor(private readonly documentService: DocumentService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleAssignee(): void {
+    this.canHaveAssignee = !this.canHaveAssignee;
   }
-
 }
