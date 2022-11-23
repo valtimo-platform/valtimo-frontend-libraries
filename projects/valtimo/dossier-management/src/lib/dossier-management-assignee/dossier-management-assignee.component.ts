35fd03955095832ce856e170435d7277a405277e
maisamaximo <maisa.maximo@ritense.com>
2022-11-23 13:14:06 +0100

    create component for assignee

---
+++ projects/valtimo/dossier-management/src/lib/dossier-management-assignee/dossier-management-assignee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'valtimo-dossier-management-assignee',
+  templateUrl: './dossier-management-assignee.component.html',
+  styleUrls: ['./dossier-management-assignee.component.css']
+})
+export class DossierManagementAssigneeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
