3161af5e489b80ca4175de13d1d38c90f6b7eda7
Maxim Buur <maxim.buur@ritense.com>
2022-11-28 12:34:50 +0100

    clean up component

--- projects/valtimo/components/src/lib/components/left-sidebar/left-sidebar.component.ts
+++ projects/valtimo/components/src/lib/components/left-sidebar/left-sidebar.component.ts
@@ -18,18 +18,14 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  EventEmitter,
   HostListener,
-  Input,
   OnDestroy,
-  OnInit,
-  Output,
   ViewChild,
 } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {BehaviorSubject, combineLatest, fromEvent, Observable, Subscription} from 'rxjs';
-import {debounceTime, filter, map, take} from 'rxjs/operators';
-import {ConfigService, CustomLeftSidebar, MenuItem} from '@valtimo/config';
+import {combineLatest, Observable, Subscription} from 'rxjs';
+import {filter, map, take} from 'rxjs/operators';
+import {ConfigService, MenuItem} from '@valtimo/config';
 import {MenuService} from '../menu/menu.service';
 import {ShellService} from '../../services/shell.service';
 import {BreakpointObserver} from '@angular/cdk/layout';
@@ -40,35 +36,10 @@ import {NavigationEnd, Router} from '@angular/router';
   templateUrl: './left-sidebar.component.html',
   styleUrls: ['./left-sidebar.component.scss'],
 })
-export class LeftSidebarComponent implements OnInit, AfterViewInit, OnDestroy {
+export class LeftSidebarComponent implements AfterViewInit, OnDestroy {
   @ViewChild('toggleButton') toggleButtonRef: ElementRef;
 
-  @Input() menuIsOpen: boolean;
-  @Output() menuWidthChanged: EventEmitter<number> = new EventEmitter();
-
-  readonly menuTooltipText$ = this.translateService.stream('interface.mainMenu');
-  readonly toggleMenuText$ = this.translateService.stream('interface.toggleMenu');
-
-  readonly mouseIsOnResizeBorder$ = new BehaviorSubject<boolean>(false);
-  readonly menuWidth$ = new BehaviorSubject<number>(undefined);
-  readonly isResizing$ = new BehaviorSubject<boolean>(false);
-
-  private bodyStyle: any;
-
-  private xOnClick: number;
-  private menuWidthOnClick: number;
-
-  private mouseXSubscription: Subscription;
-  private mouseUpSubscription: Subscription;
   private breakpointSubscription!: Subscription;
-
-  private defaultMenuWidth!: number;
-  private maxMenuWidth!: number;
-  private minMenuWidth!: number;
-
-  private readonly mouseX$ = fromEvent(document.body, 'mousemove').pipe(
-    map((e: MouseEvent) => e.pageX)
-  );
 
   readonly menuItems$: Observable<Array<MenuItem>> = this.menuService.menuItems$;
 
@@ -149,74 +120,13 @@ export class LeftSidebarComponent implements OnInit, AfterViewInit, OnDestroy {
     private readonly shellService: ShellService,
     private readonly breakpointObserver: BreakpointObserver,
     private readonly router: Router
-  ) {
-    this.bodyStyle = elementRef.nativeElement.ownerDocument.body.style;
-  }
-
-  ngOnInit(): void {
-    const customLeftSidebar = this.configService.config.customLeftSidebar;
-
-    this.setInitialWidth(customLeftSidebar);
-  }
-
+  ) {}
   ngAfterViewInit(): void {
     this.openBreakpointSubscription();
-
-    this.mouseXSubscription = combineLatest([this.mouseX$, this.isResizing$])
-      .pipe(debounceTime(3))
-      .subscribe(([x, isResizing]) => {
-        const menuOpen = this.menuIsOpen;
-
-        if (isResizing) {
-          const offSetWidth = this.xOnClick - x;
-          const newMenuWidth = this.menuWidthOnClick - offSetWidth;
-          const snapMargin = 35;
-          const snapInside = newMenuWidth < this.minMenuWidth - snapMargin;
-          const snapOutside = !menuOpen && offSetWidth < -snapMargin;
-
-          if (menuOpen && newMenuWidth <= this.maxMenuWidth && newMenuWidth >= this.minMenuWidth) {
-            this.setMenuWidth(newMenuWidth);
-          } else if (snapInside || snapOutside) {
-            this.snapMenu(snapInside ? this.menuWidthOnClick : this.defaultMenuWidth);
-          }
-        }
-      });
-
-    this.mouseUpSubscription = fromEvent(document, 'mouseup').subscribe(() => {
-      this.stopResizing();
-    });
   }
 
   ngOnDestroy(): void {
-    this.mouseXSubscription.unsubscribe();
-    this.mouseUpSubscription.unsubscribe();
     this.breakpointSubscription?.unsubscribe();
-  }
-
-  enterBorder(): void {
-    this.mouseIsOnResizeBorder$.next(true);
-  }
-
-  leaveBorder(): void {
-    this.mouseIsOnResizeBorder$.next(false);
-  }
-
-  resizeBorderClick(e: MouseEvent): void {
-    this.menuWidth$.pipe(take(1)).subscribe(menuWidth => {
-      this.menuWidthOnClick = menuWidth;
-      this.xOnClick = e.pageX;
-      this.bodyStyle.userSelect = 'none';
-      this.bodyStyle.cursor = 'col-resize';
-      this.isResizing$.next(true);
-    });
-  }
-
-  toggled(): void {
-    console.log('toggled');
-  }
-
-  checkSequence(sequence: string): void {
-    console.log('sequence', sequence);
   }
 
   navigateToRoute(route: Array<string>): void {
@@ -229,36 +139,6 @@ export class LeftSidebarComponent implements OnInit, AfterViewInit, OnDestroy {
           this.shellService.setSideBarExpanded(false);
         }
       });
-  }
-
-  private setInitialWidth(customLeftSidebar: CustomLeftSidebar | undefined): void {
-    const localMenuWidth = localStorage.getItem('menuWidth');
-    const localMenuWidthNumber = localMenuWidth ? Number(localMenuWidth) : undefined;
-
-    this.defaultMenuWidth = localMenuWidthNumber || customLeftSidebar?.defaultMenuWidth || 230;
-    this.maxMenuWidth = customLeftSidebar?.maxMenuWidth || 330;
-    this.minMenuWidth = customLeftSidebar?.minMenuWidth || 120;
-    this.setMenuWidth(this.defaultMenuWidth);
-  }
-
-  private snapMenu(snapTo: number): void {
-    this.stopResizing();
-    this.setMenuWidth(snapTo);
-    setTimeout(() => {
-      this.toggleButtonRef.nativeElement.click();
-    });
-  }
-
-  private stopResizing(): void {
-    this.bodyStyle.cursor = 'auto';
-    this.bodyStyle.userSelect = 'auto';
-    this.isResizing$.next(false);
-    localStorage.setItem('menuWidth', this.menuWidth$.getValue().toString());
-  }
-
-  private setMenuWidth(width: number): void {
-    this.menuWidth$.next(width);
-    this.menuWidthChanged.emit(width);
   }
 
   private openBreakpointSubscription(): void {
