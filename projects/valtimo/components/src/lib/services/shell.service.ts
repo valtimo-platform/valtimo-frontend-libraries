ea946110b7bb4946d0cc034c185fe96efdcc76c8
Maxim Buur <maxim.buur@ritense.com>
2023-01-18 08:26:46 +0100

    set width of main content and sidebar from shell service

--- projects/valtimo/components/src/lib/services/shell.service.ts
+++ projects/valtimo/components/src/lib/services/shell.service.ts
@@ -1,14 +1,18 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Renderer2, RendererFactory2} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShellService {
+  private renderer!: Renderer2;
   private readonly _sideBarExpanded$ = new BehaviorSubject<boolean>(true);
   private readonly _panelExpanded$ = new BehaviorSubject<boolean>(false);
   private readonly _largeScreen$ = new BehaviorSubject<boolean>(true);
   private readonly _mouseOnTopBar$ = new BehaviorSubject<boolean>(false);
+  private readonly _sidenavWidth$ = new BehaviorSubject<number>(150);
+  private readonly _sidenavElement$ = new BehaviorSubject<HTMLElement>(undefined);
+  private readonly _contentElement$ = new BehaviorSubject<HTMLElement>(undefined);
 
   get sideBarExpanded$(): Observable<boolean> {
     return this._sideBarExpanded$.asObservable();
@@ -21,6 +25,14 @@ export class ShellService {
   }
   get mouseOnTopBar$(): Observable<boolean> {
     return this._mouseOnTopBar$.asObservable();
+  }
+
+  get sidenavWidth$(): Observable<number> {
+    return this._sidenavWidth$.asObservable();
+  }
+
+  constructor(private readonly rendererFactory2: RendererFactory2) {
+    this.renderer = rendererFactory2.createRenderer(null, null);
   }
 
   toggleSideBar(): void {
@@ -47,5 +59,23 @@ export class ShellService {
 
   setMouseOnTopBar(mouseOnTopBar: boolean): void {
     this._mouseOnTopBar$.next(mouseOnTopBar);
+  }
+
+  setSidenavElement(element: HTMLElement): void {
+    this._sidenavElement$.next(element);
+    this.renderer.setStyle(
+      this._sidenavElement$.getValue(),
+      'width',
+      `${this._sidenavWidth$.getValue()}px`
+    );
+  }
+
+  setContentElement(element: HTMLElement): void {
+    this._contentElement$.next(element);
+    this.renderer.setStyle(
+      this._contentElement$.getValue(),
+      'padding-left',
+      `calc(2rem + ${this._sidenavWidth$.getValue()}px)`
+    );
   }
 }
