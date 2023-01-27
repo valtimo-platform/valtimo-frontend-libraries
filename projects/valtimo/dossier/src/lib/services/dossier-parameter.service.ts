775d642f219df95e2389fce2947a7edeb3f8e710
Maxim Buur <maxim.buur@ritense.com>
2023-01-27 10:57:25 +0100

    add missing copyright

--- projects/valtimo/dossier/src/lib/services/dossier-parameter.service.ts
+++ projects/valtimo/dossier/src/lib/services/dossier-parameter.service.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015-2020 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {Injectable, OnDestroy} from '@angular/core';
 import {DossierParameters} from '../models';
 import {
@@ -125,7 +141,7 @@ export class DossierParameterService implements OnDestroy {
   }
 
   private getUrlWithoutParams(): string {
-    let urlTree = this.router.parseUrl(this.router.url);
+    const urlTree = this.router.parseUrl(this.router.url);
     urlTree.queryParams = {};
     urlTree.fragment = null;
     return urlTree.toString();
