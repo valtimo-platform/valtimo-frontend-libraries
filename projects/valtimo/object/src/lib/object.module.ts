9c54e8d097d47e8e56d381b917980b68d8b412b6
Kevin-Snijders <kevin.snijders@ritense.com>
2023-02-13 10:08:40 +0100

    Updated edit button to carbon styling + added translations

--- projects/valtimo/object/src/lib/object.module.ts
+++ projects/valtimo/object/src/lib/object.module.ts
@@ -6,6 +6,7 @@ import {AsyncPipe, CommonModule} from '@angular/common';
 import {ListModule, WidgetModule} from '@valtimo/components';
 import {ObjectDetailContainerComponent} from './components/object-detail-container/object-detail-container.component';
 import {ObjectDetailComponent} from './components/object-detail-container/tabs/object-detail/object-detail.component';
+import {ButtonModule, IconModule} from 'carbon-components-angular';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import {ObjectDetailComponent} from './components/object-detail-container/tabs/o
     TranslateModule,
     AsyncPipe,
     WidgetModule,
-    ListModule
+    ListModule,
+    ButtonModule,
+    IconModule
   ],
   exports: [
   ]
