26abfea6c2a90e222b2a8399a17fd76104270a51
Maxim Buur <maxim.buur@ritense.com>
2023-03-03 09:55:50 +0100

    checkbox to cabron, move event stop propagation

--- projects/valtimo/user-interface/src/lib/components/modal/modal.component.ts
+++ projects/valtimo/user-interface/src/lib/components/modal/modal.component.ts
@@ -51,10 +51,6 @@ export class ModalComponent implements OnInit {
     this.modalService.closeModal();
   }
 
-  stopPropagation(event: Event): void {
-    event.stopPropagation();
-  }
-
   modalMouseEnter(): void {
     this.mouseInsideModal$.next(true);
   }
