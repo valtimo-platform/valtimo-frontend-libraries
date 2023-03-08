76eed0d2a68d0cc09d347879972387ae6b9f50d5
mritense <79840403+mritense@users.noreply.github.com>
2023-03-08 11:21:35 +0000

    front end version in sidebar with script (#422)

--- projects/valtimo/config/src/lib/constants/versions.ts
+++ projects/valtimo/config/src/lib/constants/versions.ts
@@ -14,16 +14,10 @@
  * limitations under the License.
  */
 
-export * from './config';
-export * from './email-notification-settings.model';
-export * from './menu-item.model';
-export * from './menu.config';
-export * from './security.config';
-export * from './extension.model';
-export * from './http-loader';
-export * from './roles';
-export * from './user-management.model';
-export * from './page';
-export * from './connector.model';
-export * from './search.model';
-export * from './versions.model';
+import {Versions} from '../models';
+
+const VERSIONS: Versions = {
+  frontendLibraries: '10.4.0',
+};
+
+export {VERSIONS};
