f2159e5971d72f28487639c0df15a60b31456d9e
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2023-03-22 11:26:31 +0100

    Feature/58649 catalogi api plugin new plugin action retrieve resultaattype url (#439)
    * #58639 Catalogi API plugin - new plugin action 'Retrieve Statustype URL'
    
    * #58639 Support process-link on bpmnCallActivity
    
    * #58649 & #58651 Catalogi API actions 'Retrieve Besluittype' & 'Retrieve Resultaattype'
    
    * fix linting
    
    * fix linting

---
+++ projects/valtimo/plugin/src/lib/plugins/catalogi-api/components/get-besluittype/get-besluittype-configuration.component.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {FunctionConfigurationComponent} from '../../../../models';
+import {BehaviorSubject, combineLatest, Observable, Subscription, take} from 'rxjs';
+import {GetBesluittypeConfig} from '../../models';
+
+@Component({
+  selector: 'valtimo-set-besluittype-configuration',
+  templateUrl: './get-besluittype-configuration.component.html',
+  styleUrls: ['./get-besluittype-configuration.component.scss'],
+})
+export class GetBesluittypeConfigurationComponent
+  implements FunctionConfigurationComponent, OnInit, OnDestroy
+{
+  @Input() save$: Observable<void>;
+  @Input() disabled$: Observable<boolean>;
+  @Input() pluginId: string;
+  @Input() prefillConfiguration$: Observable<GetBesluittypeConfig>;
+  @Output() valid: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() configuration: EventEmitter<GetBesluittypeConfig> =
+    new EventEmitter<GetBesluittypeConfig>();
+
+  private saveSubscription!: Subscription;
+
+  private readonly formValue$ = new BehaviorSubject<GetBesluittypeConfig | null>(null);
+  private readonly valid$ = new BehaviorSubject<boolean>(false);
+
+  ngOnInit(): void {
+    this.openSaveSubscription();
+  }
+
+  ngOnDestroy() {
+    this.saveSubscription?.unsubscribe();
+  }
+
+  formValueChange(formValue: GetBesluittypeConfig): void {
+    this.formValue$.next(formValue);
+    this.handleValid(formValue);
+  }
+
+  private handleValid(formValue: GetBesluittypeConfig): void {
+    const valid = !!(formValue.besluittype && formValue.processVariable);
+
+    this.valid$.next(valid);
+    this.valid.emit(valid);
+  }
+
+  private openSaveSubscription(): void {
+    this.saveSubscription = this.save$?.subscribe(save => {
+      combineLatest([this.formValue$, this.valid$])
+        .pipe(take(1))
+        .subscribe(([formValue, valid]) => {
+          if (valid) {
+            this.configuration.emit(formValue);
+          }
+        });
+    });
+  }
+}
