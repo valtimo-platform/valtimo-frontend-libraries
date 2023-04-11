ae195c3425179d2acc7af218033e14cd38c19143
Kevin-Snijders <66664831+Kevin-Snijders@users.noreply.github.com>
2023-04-11 15:30:16 +0200

    Feature/story 58798 (#447)
    * Add error handler if the API to retrieve the list values is not working
    
    * The modal is no longer closed and cleared when incorrect data is being submitted
    
    * Don't show formio input if there is no definition
    
    * Disabled input if form is not available

--- projects/valtimo/object/src/lib/components/object-list/object-list.component.ts
+++ projects/valtimo/object/src/lib/components/object-list/object-list.component.ts
@@ -18,7 +18,7 @@ import {Component} from '@angular/core';
 import {
   BehaviorSubject,
   combineLatest,
-  distinctUntilChanged,
+  distinctUntilChanged, filter,
   map,
   Observable,
   of,
@@ -117,6 +117,11 @@ export class ObjectListComponent {
     this.refreshObjectList$,
   ]).pipe(
     switchMap(([objectManagementId, currentPage, columnType, searchFieldValues]) => {
+      const handleError = () => {
+        this.disableInput();
+        return of(null);
+      };
+
       if (columnType === ColumnType.CUSTOM) {
         return this.objectService.postObjectsByObjectManagementId(
           objectManagementId,
@@ -127,22 +132,28 @@ export class ObjectListComponent {
           Object.keys(searchFieldValues).length > 0
             ? {otherFilters: this.mapSearchValuesToFilters(searchFieldValues)}
             : {}
+        ).pipe(
+          catchError(() => handleError())
         );
       } else {
         return this.objectService.getObjectsByObjectManagementId(objectManagementId, {
           page: currentPage.page,
           size: currentPage.size,
-        });
+        }).pipe(
+          catchError(() => handleError())
+        );
       }
     }),
     tap(instanceRes => {
-      this.pageSizes$.pipe(take(1)).subscribe(sizes => {
-        // @ts-ignore
-        this.pageSizes$.next({...sizes, collectionSize: instanceRes.totalElements});
-      });
+      if (instanceRes != null) {
+        this.pageSizes$.pipe(take(1)).subscribe(sizes => {
+          // @ts-ignore
+          this.pageSizes$.next({...sizes, collectionSize: instanceRes.totalElements});
+        });
+      }
     }),
     map(res =>
-      res.content.map(record =>
+      res?.content?.map(record =>
         record?.items?.reduce(
           (obj, item) => Object.assign(obj, {objectId: record.id}, {[item.key]: item.value}),
           {}
@@ -239,28 +250,32 @@ export class ObjectListComponent {
     }
   }
 
-  addObject(): void {
+  public addObject(): void {
     this.disableInput();
     combineLatest([this.objectManagementId$, this.submission$, this.formValid$])
-      .pipe(take(1))
-      .subscribe(([objectManagementId, submission, formValid]) => {
-        if (formValid) {
-          submission = this.objectService.removeEmptyStringValuesFromSubmission(submission);
-          this.objectService
-            .createObject({objectManagementId}, {...submission})
+      .pipe(
+        take(1),
+        filter(([objectManagementId, submission, formValid]) => formValid),
+        switchMap(([objectManagementId, submission]) =>
+          this.objectService.createObject({ objectManagementId }, { ...this.objectService.removeEmptyStringValuesFromSubmission(submission) })
             .pipe(
               take(1),
-              catchError((error: any) => this.handleCreateObjectError(error)),
-              finalize(() => {
-                this.enableInput();
+              catchError((error: any) => {
+                this.handleCreateObjectError(error);
+                return throwError(error);
               })
             )
-            .subscribe(() => {
-              this.closeModal();
-              this.refreshObjectList();
-              this.clearForm$.next(true);
-              this.toastr.success(this.translate.instant('object.messages.objectCreated'));
-            });
+        ),
+        finalize(() => {
+          this.enableInput();
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.closeModal();
+          this.refreshObjectList();
+          this.clearForm$.next(true);
+          this.toastr.success(this.translate.instant('object.messages.objectCreated'));
         }
       });
   }
@@ -304,7 +319,6 @@ export class ObjectListComponent {
   }
 
   private handleCreateObjectError(error: any) {
-    this.closeModal();
     this.toastr.error(this.translate.instant('object.messages.objectCreationError'));
     return throwError(error);
   }
