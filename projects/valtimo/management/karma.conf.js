1dd6d3b1a85a6a3227320aaf369e57da204d2b41
Maxim Buur <maxim.buur@ritense.com>
2023-04-12 16:11:13 +0200

    set up basic unit testing for dossier list

--- projects/valtimo/management/karma.conf.js
+++ projects/valtimo/management/karma.conf.js
@@ -17,6 +17,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -24,6 +26,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
@@ -41,7 +44,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['Chrome', 'ChromeHeadless'],
+    singleRun: true,
+    failOnEmptyTestSuite: false,
   });
 };
