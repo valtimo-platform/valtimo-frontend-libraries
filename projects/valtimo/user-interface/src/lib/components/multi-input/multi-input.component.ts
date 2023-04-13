1a8b41c56077a9ac15b0499f75a8be135ef77e24
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2023-04-13 10:08:53 +0200

    #61803 More than 20 entries Template-data can't be set in SmartDocuments-Document genereren plugin action

--- projects/valtimo/user-interface/src/lib/components/multi-input/multi-input.component.ts
+++ projects/valtimo/user-interface/src/lib/components/multi-input/multi-input.component.ts
@@ -32,7 +32,7 @@ export class MultiInputComponent implements OnInit, OnDestroy {
   @Input() type: MultiInputType = 'value';
   @Input() initialAmountOfRows = 1;
   @Input() minimumAmountOfRows = 1;
-  @Input() maxRows = 20;
+  @Input() maxRows = null;
   @Input() addRowText = '';
   @Input() addRowTranslationKey = '';
   @Input() deleteRowText = '';
@@ -59,7 +59,7 @@ export class MultiInputComponent implements OnInit, OnDestroy {
     )
   );
   readonly addRowEnabled$: Observable<boolean> = this.values$.pipe(
-    map(values => !!(values.length < this.maxRows))
+    map(values => !!(this.maxRows === null || values.length < this.maxRows))
   );
 
   private valuesSubscription!: Subscription;
