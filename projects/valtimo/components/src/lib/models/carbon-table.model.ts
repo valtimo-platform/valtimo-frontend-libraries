a807158543b0cb1a8266933ba0354408619885ef
teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
2023-06-09 11:34:39 +0200

    Added CarbonTable component (#493)

---
+++ projects/valtimo/components/src/lib/models/carbon-table.model.ts
@@ -0,0 +1,48 @@
+import {TemplateRef} from '@angular/core';
+import {TableRowSize} from 'carbon-components-angular';
+
+export enum ColumnType {
+  ACTION = 'action',
+  TEMPLATE = 'template',
+  TEXT = 'text',
+}
+
+export interface CarbonTableConfig {
+  fields: ColumnConfig[];
+  enableSingleSelect?: boolean;
+  searchable?: boolean;
+  showSelectionColumn?: boolean;
+  size?: TableRowSize;
+  skeleton?: boolean;
+  sortable?: boolean;
+  striped?: boolean;
+}
+
+export interface ActionItem {
+  actionName: string;
+  callback: (_) => void;
+}
+
+export interface ColumnConfig {
+  fieldName: string;
+  fieldLabel: string;
+  columnType: ColumnType;
+  actions?: ActionItem[];
+  template?: TemplateRef<any>;
+}
+
+const defaultTableConfig: CarbonTableConfig = {
+  fields: [],
+  enableSingleSelect: false,
+  searchable: false,
+  size: 'md',
+  showSelectionColumn: true,
+  sortable: true,
+  skeleton: false,
+  striped: false,
+};
+
+export const createCarbonTableConfig = (config: CarbonTableConfig): CarbonTableConfig => ({
+  ...defaultTableConfig,
+  ...config,
+});
--- projects/valtimo/components/src/public_api.ts
+++ projects/valtimo/components/src/public_api.ts
@@ -42,6 +42,9 @@ export * from './lib/components/right-sidebar/right-sidebar.module';
 export * from './lib/components/left-sidebar/left-sidebar.component';
 export * from './lib/components/left-sidebar/left-sidebar.module';
 
+export * from './lib/components/carbon-table/carbon-table.component';
+export * from './lib/components/carbon-table/carbon-table.module';
+
 export * from './lib/components/list/list.component';
 export * from './lib/components/list/ListFilterPipe.directive';
 export * from './lib/components/list/list.module';
