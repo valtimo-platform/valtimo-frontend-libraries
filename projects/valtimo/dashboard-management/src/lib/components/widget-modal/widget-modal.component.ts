3f63311dad703545a8c041cae28c4a8172e71f39
Maxim Buur <maxim.buur@ritense.com>
2023-06-19 13:36:52 +0200

    work on data source

--- projects/valtimo/dashboard-management/src/lib/components/widget-modal/widget-modal.component.ts
+++ projects/valtimo/dashboard-management/src/lib/components/widget-modal/widget-modal.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {BehaviorSubject, map, Observable, Subscription} from 'rxjs';
+import {BehaviorSubject, combineLatest, map, Observable, Subscription} from 'rxjs';
 import {WidgetModalType} from '../../models';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ListItem} from 'carbon-components-angular';
@@ -17,9 +17,16 @@ export class WidgetModalComponent implements OnInit, OnDestroy {
 
   public form!: FormGroup;
   public readonly open$ = new BehaviorSubject<boolean>(false);
-  public readonly dataSourceItems$: Observable<Array<ListItem>> = this.translateService
-    .stream('key')
-    .pipe(map(() => widgetDataSourcesMock.map(mockItem => ({content: mockItem, selected: false}))));
+
+  private readonly _dataSourceItems$ = new BehaviorSubject<Array<string>>([]);
+  public readonly dataSourceItems$: Observable<Array<ListItem>> = combineLatest([
+    this._dataSourceItems$,
+    this.translateService.stream('key'),
+  ]).pipe(
+    map(([dataSourceItems]) =>
+      dataSourceItems.map(mockItem => ({content: mockItem, selected: false}))
+    )
+  );
   private _openSubscription!: Subscription;
 
   get title() {
@@ -38,6 +45,7 @@ export class WidgetModalComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
+    this.setDataSourceItems();
     this.openOpenSubscription();
     this.setForm();
   }
@@ -49,11 +57,20 @@ export class WidgetModalComponent implements OnInit, OnDestroy {
   closeModal(): void {
     this.open$.next(false);
     this.form.reset();
+    this.setDataSourceItems();
   }
 
   save(): void {}
 
   delete(): void {}
+
+  dataSourceSelected(dataSource: any): void {
+    this.dataSource.setValue(dataSource?.item?.content);
+  }
+
+  private setDataSourceItems(): void {
+    this._dataSourceItems$.next(widgetDataSourcesMock);
+  }
 
   private setForm(): void {
     this.form = this.fb.group({
