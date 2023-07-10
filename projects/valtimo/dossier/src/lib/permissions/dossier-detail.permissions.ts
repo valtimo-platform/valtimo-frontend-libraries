73dfcc5be5c826f093b7f66d754313e0d2c74c0a
Teodor Dragoi <teodor.dragoi@ritense.com>
2023-07-10 12:10:18 +0300

    Integrated PBAC for Add, Edit, Delete Note

--- projects/valtimo/dossier/src/lib/permissions/dossier-detail.permissions.ts
+++ projects/valtimo/dossier/src/lib/permissions/dossier-detail.permissions.ts
@@ -1,12 +1,16 @@
 import {PermissionRequest} from '@valtimo/access-control';
 
 enum PERMISSION_ACTION {
-  claim = 'CLAIM',
+  add = 'CREATE',
   assign = 'ASSIGN',
+  claim = 'CLAIM',
+  delete = 'DELETE',
+  edit = 'MODIFY',
 }
 
 enum DOSSIER_DETAIL_PERMISSION_RESOURCE {
   domain = 'com.ritense.document.domain.impl.JsonSchemaDocument',
+  note = 'com.ritense.note.domain.Note',
 }
 
 const CAN_CLAIM_CASE_PERMISSION: PermissionRequest = {
@@ -14,9 +18,31 @@ const CAN_CLAIM_CASE_PERMISSION: PermissionRequest = {
   resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.domain,
 };
 
+const CAN_ADD_NOTE_PERMISSION: PermissionRequest = {
+  action: PERMISSION_ACTION.add,
+  resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.note,
+};
+
+const CAN_DELETE_NOTE_PERMISSION: PermissionRequest = {
+  action: PERMISSION_ACTION.delete,
+  resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.note,
+};
+
+const CAN_EDIT_NOTE_PERMISSION: PermissionRequest = {
+  action: PERMISSION_ACTION.edit,
+  resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.note,
+};
+
 const CAN_ASSIGN_CASE_PERMISSION: PermissionRequest = {
   action: PERMISSION_ACTION.assign,
   resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.domain,
 };
 
-export {CAN_CLAIM_CASE_PERMISSION, CAN_ASSIGN_CASE_PERMISSION, DOSSIER_DETAIL_PERMISSION_RESOURCE};
+export {
+  CAN_ADD_NOTE_PERMISSION,
+  CAN_ASSIGN_CASE_PERMISSION,
+  CAN_CLAIM_CASE_PERMISSION,
+  CAN_DELETE_NOTE_PERMISSION,
+  CAN_EDIT_NOTE_PERMISSION,
+  DOSSIER_DETAIL_PERMISSION_RESOURCE,
+};
