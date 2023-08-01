c53e7da94998716716fccf19bd3453da3e033c0b
Maxim Buur <maxim.buur@ritense.com>
2023-08-01 10:21:51 +0200

    revert

--- projects/valtimo/dashboard/src/lib/display-types/bar-chart/bar-chart.specification.ts
+++ projects/valtimo/dashboard/src/lib/display-types/bar-chart/bar-chart.specification.ts
@@ -20,7 +20,7 @@ import {BarChartDisplayComponent} from './components/bar-chart-display/bar-chart
 export const barChartSpecification: DisplayTypeSpecification = {
   displayTypeKey: 'bar-chart',
   displayComponent: BarChartDisplayComponent,
-  width: 2,
-  height: 1,
+  width: 1,
+  height: 2,
   translations: {},
 };
