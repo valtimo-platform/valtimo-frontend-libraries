57442ab7299e73d924992b0e0a56781e67ccf924
mritense <79840403+mritense@users.noreply.github.com>
2023-08-08 14:59:12 +0200

    single widget config update endpoint impl. (#540)

--- projects/valtimo/dashboard-management/src/lib/components/widget-modal/widget-modal.component.ts
+++ projects/valtimo/dashboard-management/src/lib/components/widget-modal/widget-modal.component.ts
@@ -224,9 +224,9 @@ export class WidgetModalComponent implements OnInit, OnDestroy {
                 this.dashboard.key,
                 widgetUpdateObject
               )
-            : this.dashboardManagementService.updateDashboardWidgetConfigurations(
+            : this.dashboardManagementService.updateDashboardWidgetConfiguration(
                 this.dashboard.key,
-                [{...widgetUpdateObject, key: this.widgetKey}]
+                {...widgetUpdateObject, key: this.widgetKey}
               )
         )
       )
