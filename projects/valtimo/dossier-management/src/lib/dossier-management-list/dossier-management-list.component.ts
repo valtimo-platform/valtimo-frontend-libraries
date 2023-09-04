9d2cc3769f90136c651ac8164bcccc94ee3acf70
Theo <theo.beers@ritense.com>
2023-09-04 15:16:48 +0200

    replaced the filteredOnRole=false argument with a management endpoint

--- projects/valtimo/dossier-management/src/lib/dossier-management-list/dossier-management-list.component.ts
+++ projects/valtimo/dossier-management/src/lib/dossier-management-list/dossier-management-list.component.ts
@@ -61,7 +61,7 @@ export class DossierManagementListComponent {
 
   private getDocumentDefinitions() {
     this.documentService
-      .queryDefinitions({filteredOnRole: false, page: this.pageParam, size: this.pagination.size})
+      .queryDefinitionsForManagement({page: this.pageParam, size: this.pagination.size})
       .subscribe((documentDefinitionPage: Page<DocumentDefinition>) => {
         this.pagination.collectionSize = documentDefinitionPage.totalElements;
         this.dossiers = documentDefinitionPage.content;
