b1939032f7659e87c4372968a56d95481fd8dc80
Maxim Buur <maxim.buur@ritense.com>
2023-11-06 12:30:59 +0100

    fix all unit tests

--- projects/valtimo/form-management/karma.conf.js
+++ projects/valtimo/form-management/karma.conf.js
@@ -17,8 +17,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,24 +25,24 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: true,
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true,
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/valtimo-angular-console'),
+      subdir: '.',
+      reporters: [{type: 'html'}, {type: 'text-summary'}],
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
-    singleRun: true,
+    browsers: ['Chrome'],
+    restartOnFileChange: true,
     failOnEmptyTestSuite: false,
   });
 };
