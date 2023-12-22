f6bc5c1c47b4ea22b16fc0dc48ff53bff83466f2
Maxim Buur <maxim.buur@ritense.com>
2023-12-22 13:33:00 +0100

    -

--- projects/valtimo/carbon/src/lib/components/tile/tile.component.ts
+++ projects/valtimo/carbon/src/lib/components/tile/tile.component.ts
@@ -20,7 +20,7 @@ import {v4 as uuidv4} from 'uuid';
   exportAs: 'vcdsTile',
   template: `
     <Tile [id]="tileId" #reactNode>
-      <ng-content></ng-content>
+      <ReactContent><ng-content></ng-content></ReactContent>
     </Tile>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
