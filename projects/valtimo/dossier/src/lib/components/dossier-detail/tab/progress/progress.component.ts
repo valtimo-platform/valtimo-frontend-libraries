5228026165eb24f6d20e147beae9138ae586266b
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2024-01-02 15:36:57 +0100

    #87470 PR comments

--- projects/valtimo/dossier/src/lib/components/dossier-detail/tab/progress/progress.component.ts
+++ projects/valtimo/dossier/src/lib/components/dossier-detail/tab/progress/progress.component.ts
@@ -15,7 +15,7 @@
  */
 
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 import {DocumentService, ProcessDocumentInstance} from '@valtimo/document';
 import {BehaviorSubject, combineLatest, map, Observable, of, switchMap, tap} from 'rxjs';
 import {ListItem} from 'carbon-components-angular/dropdown';
@@ -26,31 +26,32 @@ import {ListItem} from 'carbon-components-angular/dropdown';
   styleUrls: ['./progress.component.css'],
 })
 export class DossierDetailTabProgressComponent {
-  private readonly processDocumentInstances$: Observable<Array<ProcessDocumentInstance>> = of(
-    this.route.snapshot.paramMap.get('documentId')
-  ).pipe(
-    switchMap(documentId => this.documentService.findProcessDocumentInstances(documentId)),
-    map(processDocumentInstances =>
-      processDocumentInstances.map(processDocumentInstance => ({
-        ...processDocumentInstance,
-        startedOn: new Date(processDocumentInstance.startedOn);
-      }))
-    ),
-    map(processDocumentInstances =>
-      processDocumentInstances.sort((a, b) =>
-        a.isActive === b.isActive
-          ? b.startedOn.getTime() - a.startedOn.getTime()
-          : a.isActive
-          ? -1
-          : 1
-      )
-    ),
-    tap(processDocumentInstances => {
-      if (processDocumentInstances.length > 0) {
-        this.selectedProcessInstanceId$.next(processDocumentInstances[0].id.processInstanceId);
-      }
-    })
-  );
+  private readonly processDocumentInstances$: Observable<Array<ProcessDocumentInstance>> =
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.documentService.findProcessDocumentInstances(params.get('documentId'))
+      ),
+      map(processDocumentInstances =>
+        processDocumentInstances.map(processDocumentInstance => ({
+          ...processDocumentInstance,
+          startedOn: new Date(processDocumentInstance.startedOn),
+        }))
+      ),
+      map(processDocumentInstances =>
+        processDocumentInstances.sort((a, b) =>
+          a.isActive === b.isActive
+            ? b.startedOn.getTime() - a.startedOn.getTime()
+            : a.isActive
+            ? -1
+            : 1
+        )
+      ),
+      tap(processDocumentInstances => {
+        if (processDocumentInstances.length > 0) {
+          this.selectedProcessInstanceId$.next(processDocumentInstances[0].id.processInstanceId);
+        }
+      })
+    );
 
   public readonly processInstanceItems$: Observable<Array<ListItem>> =
     this.processDocumentInstances$.pipe(
