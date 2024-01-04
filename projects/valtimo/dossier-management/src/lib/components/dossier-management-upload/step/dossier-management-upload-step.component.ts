6abe444833b34f1bed59b39431805910af6ab014
teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
2024-01-04 14:09:31 +0100

    Feature/89485 import cases (#766)
    * Added importing case definition

---
+++ projects/valtimo/dossier-management/src/lib/components/dossier-management-upload/step/dossier-management-upload-step.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'valtimo-dossier-management-upload-step',
+  templateUrl: './dossier-management-upload-step.component.html',
+  styleUrls: ['./dossier-management-upload-step.component.scss'],
+})
+export class DossierManagementUploadStepComponent {
+  @Input() illustration!: string;
+}
