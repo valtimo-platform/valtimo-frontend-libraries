4afaf09061e8334055a07f547bd88e2f4247395e
Maxim Buur <maxim.buur@ritense.com>
2024-01-11 17:07:52 +0100

    add zone offset http interceptor

---
+++ projects/valtimo/security/src/lib/interceptors/zone-offset.interceptor.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import moment from 'moment';
+
+@Injectable()
+export class ZoneOffsetInterceptor implements HttpInterceptor {
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const modifiedReq = req.clone({
+      headers: req.headers.set('Zone-Offset', this.getFormattedZoneOffset()),
+    });
+    return next.handle(modifiedReq);
+  }
+
+  private getFormattedZoneOffset(): string {
+    let offset = new Date().getTimezoneOffset();
+    let isNegative = false;
+
+    if (offset < 0) {
+      isNegative = true;
+      offset = offset * -1;
+    }
+
+    const duration = moment.duration(offset, 'minutes');
+    const momentOffset = moment.utc(duration.asMilliseconds()).format('HH:mm');
+
+    return `${isNegative ? '+' : '-'}${momentOffset}`;
+  }
+}
