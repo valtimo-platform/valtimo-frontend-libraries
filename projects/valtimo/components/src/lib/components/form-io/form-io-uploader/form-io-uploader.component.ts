bbe419667230d9d83b22ae2057bd1ba959b1ebaa
Maxim Buur <maxim.buur@ritense.com>
2024-01-24 13:10:23 +0100

    test remove state services

--- projects/valtimo/components/src/lib/components/form-io/form-io-uploader/form-io-uploader.component.ts
+++ projects/valtimo/components/src/lib/components/form-io/form-io-uploader/form-io-uploader.component.ts
@@ -18,9 +18,6 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormioCustomComponent} from '@formio/angular';
 import {BehaviorSubject} from 'rxjs';
 import {DownloadService, ResourceDto, ResourceFile, UploadProviderService} from '@valtimo/resource';
-import {FormIoStateService} from '../services/form-io-state.service';
-import {take} from 'rxjs/operators';
-import {FormIoDomService} from '../services/form-io-dom.service';
 
 @Component({
   selector: 'valtimo-form-io-uploader',
@@ -41,8 +38,6 @@ export class FormIoUploaderComponent implements FormioCustomComponent<Array<Reso
 
   constructor(
     private readonly uploadProviderService: UploadProviderService,
-    private readonly stateService: FormIoStateService,
-    private readonly domService: FormIoDomService,
     private readonly downloadService: DownloadService
   ) {}
 
@@ -68,20 +63,16 @@ export class FormIoUploaderComponent implements FormioCustomComponent<Array<Reso
   }
 
   fileSelected(file: File) {
-    this.domService.toggleSubmitButton(true);
     this.uploading$.next(true);
-    this.stateService.documentDefinitionName$.pipe(take(1)).subscribe(name => {
-      this.uploadProviderService.uploadFile(file, name).subscribe(result => {
-        this.domService.toggleSubmitButton(false);
-        this.uploading$.next(false);
-        this._value.push(result);
-        this.valueChange.emit(this._value);
-      });
+    const name = 'test';
+    this.uploadProviderService.uploadFile(file, name).subscribe(result => {
+      this.uploading$.next(false);
+      this._value.push(result);
+      this.valueChange.emit(this._value);
     });
   }
 
   deleteFile(resourceId: string): void {
-    this.domService.toggleSubmitButton(true);
     this._value = this._value.filter((file: ResourceFile) =>
       file?.data ? file?.data?.resourceId !== resourceId : true
     );
