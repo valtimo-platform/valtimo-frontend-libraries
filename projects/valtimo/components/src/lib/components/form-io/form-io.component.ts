171f122afd2b135889b752acde708fb698a7a02e
Maxim Buur <maxim.buur@ritense.com>
2024-01-24 17:03:30 +0100

    remove reloadform

--- projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
+++ projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
@@ -144,7 +144,6 @@ export class FormioComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   public formReady(form: FormIoSourceComponent): void {
-    this.reloadForm();
     this.stateService.currentForm = form;
   }
 
@@ -178,12 +177,6 @@ export class FormioComponent implements OnInit, OnChanges, OnDestroy {
         });
       })
     );
-  }
-
-  private reloadForm(): void {
-    this.form$.pipe(take(1)).subscribe(form => {
-      this.form$.next(form);
-    });
   }
 
   private scrollToTop(): void {
