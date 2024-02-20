2f6d2da882f53aab01fb271775aaa64447bbd0e5
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2024-02-20 13:52:55 +0100

    Added export for end-hersteltermijn component (#852)

--- projects/valtimo/plugin/src/public-api.ts
+++ projects/valtimo/plugin/src/public-api.ts
@@ -63,6 +63,7 @@ export * from './lib/plugins/zaken-api/components/create-niet-natuurlijk-persoon
 export * from './lib/plugins/zaken-api/components/create-zaak/create-zaak-configuration.component';
 export * from './lib/plugins/zaken-api/components/set-zaakopschorting/set-zaakopschorting.component';
 export * from './lib/plugins/zaken-api/components/start-hersteltermijn/start-hersteltermijn-configuration.component';
+export * from './lib/plugins/zaken-api/components/end-hersteltermijn/end-hersteltermijn.component';
 /* objecten api plugin */
 export * from './lib/plugins/objecten-api/objecten-api-plugin-module';
 export * from './lib/plugins/objecten-api/objecten-api-plugin.specification';
