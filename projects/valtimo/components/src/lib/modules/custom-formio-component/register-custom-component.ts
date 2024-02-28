fe87714507831639686067eaa242393775598c33
Maxim Buur <maxim.buur@ritense.com>
2024-02-28 12:05:23 +0100

    stash

---
+++ projects/valtimo/components/src/lib/modules/custom-formio-component/register-custom-component.ts
@@ -0,0 +1,43 @@
+import {Injector, Type} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
+import {Components} from '@formio/angular';
+import {FormioCustomComponentInfo} from './elements.common';
+import {createCustomFormioComponent} from './create-custom-component';
+import {CustomTagsService} from './custom-tags.service';
+
+export function registerCustomTag(tag: string, injector: Injector): void {
+  injector.get(CustomTagsService).addCustomTag(tag);
+}
+
+export function registerCustomTags(tags: string[], injector: Injector): void {
+  tags.forEach(tag => registerCustomTag(tag, injector));
+}
+
+export function registerCustomFormioComponent(
+  options: FormioCustomComponentInfo,
+  angularComponent: Type<any>,
+  injector: Injector
+): void {
+  registerCustomTag(options.selector, injector);
+
+  const complexCustomComponent = createCustomElement(angularComponent, {injector});
+  // @ts-ignore
+  customElements.define(options.selector, complexCustomComponent);
+
+  Components.setComponent(options.type, createCustomFormioComponent(options));
+}
+
+export function registerCustomFormioComponentWithClass(
+  options: FormioCustomComponentInfo,
+  angularComponent: Type<any>,
+  formioClass: any,
+  injector: Injector
+): void {
+  registerCustomTag(options.selector, injector);
+
+  const complexCustomComponent = createCustomElement(angularComponent, {injector});
+  // @ts-ignore
+  customElements.define(options.selector, complexCustomComponent);
+
+  Components.setComponent(options.type, formioClass);
+}
