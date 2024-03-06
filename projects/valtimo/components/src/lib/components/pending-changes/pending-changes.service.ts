8a6f9757acea4c845fe6fcd6a4891884ec55482e
teodor-ritense <teodor.dragoi@ritense.com>
2024-03-06 16:28:25 +0100

    Remove unnecessary imports

--- projects/valtimo/components/src/lib/components/pending-changes/pending-changes.service.ts
+++ projects/valtimo/components/src/lib/components/pending-changes/pending-changes.service.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
