218336b773940ebf726617cf3df1d1469475dbeb
Maxim Buur <maxim.buur@ritense.com>
2024-03-13 13:50:24 +0100

    list of cases

--- projects/valtimo/task-management/src/lib/components/task-management/task-management.component.ts
+++ projects/valtimo/task-management/src/lib/components/task-management/task-management.component.ts
@@ -15,12 +15,70 @@
  */
 
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CarbonListModule, ColumnConfig, Pagination, ViewType} from '@valtimo/components';
+import {BehaviorSubject, map, Observable, switchMap} from 'rxjs';
+import {DocumentDefinition, DocumentService, Page} from '@valtimo/document';
+import moment from 'moment/moment';
+import {CommonModule} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'valtimo-task-management',
   templateUrl: './task-management.component.html',
   styleUrls: ['./task-management.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, CarbonListModule, TranslateModule],
 })
-export class TaskManagementComponent {}
+export class TaskManagementComponent {
+  public pagination: Pagination = {
+    collectionSize: 0,
+    page: 1,
+    size: 10,
+  };
+
+  private readonly _refreshData$ = new BehaviorSubject<null>(null);
+
+  public readonly dossiers$: Observable<DocumentDefinition[]> = this._refreshData$.pipe(
+    switchMap(() =>
+      this.documentService.queryDefinitionsForManagement({
+        page: this.pagination.page - 1,
+        size: this.pagination.size,
+      })
+    ),
+    map((documentDefinitionPage: Page<DocumentDefinition>) => {
+      this.pagination = {
+        ...this.pagination,
+        collectionSize: documentDefinitionPage.totalElements,
+      };
+
+      return documentDefinitionPage.content.map((documentDefinition: DocumentDefinition) => ({
+        ...documentDefinition,
+        createdOn: moment(documentDefinition.createdOn).format('DD MMM YYYY HH:mm'),
+      }));
+    })
+  );
+
+  public readonly dossierFields: ColumnConfig[] = [
+    {key: 'schema.title', label: 'fieldLabels.caseName', viewType: ViewType.TEXT},
+  ];
+
+  constructor(
+    private readonly documentService: DocumentService,
+    private readonly router: Router
+  ) {}
+
+  public paginationClicked(page: number): void {
+    this.pagination = {...this.pagination, page};
+    this._refreshData$.next(null);
+  }
+
+  public paginationSet(size: number): void {
+    this.pagination = {...this.pagination, size};
+    this._refreshData$.next(null);
+  }
+
+  public redirectToDetails(documentDefinition: DocumentDefinition): void {
+    this.router.navigate(['/dossier-management/dossier', documentDefinition.id.name]);
+  }
+}
