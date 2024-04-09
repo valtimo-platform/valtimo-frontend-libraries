9d710d2334e4bcc36de5e0f6fff2a78b303d0987
teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
2024-04-09 12:29:49 +0200

    Add documenten api boilerplate (#932)

---
+++ projects/valtimo/documenten-api/src/lib/components/documenten-api/documenten-api.component.ts
@@ -0,0 +1,8 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'valtimo-documenten-api',
+  templateUrl: './documenten-api.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DocumentenApiComponent {}
