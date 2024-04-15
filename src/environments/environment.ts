51f95cbc7b64ef536d7c3cc5c5c5dab627883ee3
Sofia Ivars <sofia.ivars@ritense.com>
2024-04-15 12:39:16 +0200

    Removed fileReplacements text for environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,9 +14,6 @@
  * limitations under the License.
  */
 
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 import {NgxLoggerLevel} from 'ngx-logger';
 import {
   DefinitionColumn,
@@ -256,6 +253,7 @@ export const environment: ValtimoConfig = {
     allowUserThemeSwitching: true,
     enableCompactModeToggle: true,
     enableUserNameInTopBarToggle: true,
+    enableObjectManagement: true
   },
   customDossierHeader: {
     leningen: [
