2a21aa61ff09284204095f2e55d6149049543031
Thomas Minke <thomas.minke@ritense.com>
2024-04-25 14:11:02 +0200

    Changed jwt decode function

--- projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
+++ projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
@@ -206,7 +206,7 @@ export class FormioComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private setToken(token: string): void {
-    Formio.setUser(jwtDecode(token));
+    Formio.setUser(jwt_decode(token));
     Formio.setToken(token);
     localStorage.setItem(this._FORMIO_TOKEN_LOCAL_STORAGE_KEY, token);
     this.setTimerForTokenRefresh(token);
