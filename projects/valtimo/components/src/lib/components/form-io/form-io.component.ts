9221f9597d42b5f1d3e5151794af8e3d7563dfa1
Ivar Koreman <97440425+IvarKoreman-Ritense@users.noreply.github.com>
2024-04-25 09:10:42 +0200

    TP Story #110428 - [FE] Exposed JWT token to form.io (#955)
    - Set Formio's `projectUrl` and `authUrl to the current origin, so that api.form.io no longer is used as a default.
    - Set the user in Formio so that the `/current` endpoint is no longer called.

--- projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
+++ projects/valtimo/components/src/lib/components/form-io/form-io.component.ts
@@ -125,6 +125,9 @@ export class FormioComponent implements OnInit, OnChanges, OnDestroy {
   ) {}
 
   public ngOnInit(): void {
+    Formio.setProjectUrl(location.origin);
+    Formio.authUrl = location.origin;
+
     this.openRouteSubscription();
     this.errors$.next([]);
     this.setInitialToken();
@@ -203,6 +206,7 @@ export class FormioComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private setToken(token: string): void {
+    Formio.setUser(jwtDecode(token));
     Formio.setToken(token);
     localStorage.setItem(this._FORMIO_TOKEN_LOCAL_STORAGE_KEY, token);
     this.setTimerForTokenRefresh(token);
