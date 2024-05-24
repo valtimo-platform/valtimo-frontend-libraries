58453be2e95a42fa6f0bac60e26da303c212c5fb
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2024-05-24 09:28:17 +0200

    Task polling and ordering

--- projects/valtimo/process/src/lib/models/process.model.ts
+++ projects/valtimo/process/src/lib/models/process.model.ts
@@ -99,7 +99,8 @@ export interface ProcessInstanceTask {
   assignee: string;
   created: string;
   createdUnix: number;
-  due: string;
+  due?: string;
+  dueUnix?: number;
   followUp: string;
   delegationState: string;
   description: string;
