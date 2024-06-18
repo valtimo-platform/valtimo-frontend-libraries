eb88f5a25a25bdc63378f15dab1e4bff16c0f3e9
teodor-ritense <teodor.dragoi@ritense.com>
2024-06-18 14:30:49 +0200

    Add translations for display types

--- projects/valtimo/dossier-management/src/lib/components/dossier-management-widget-configurators/fields/column/dossier-management-widget-fields-column.component.ts
+++ projects/valtimo/dossier-management/src/lib/components/dossier-management-widget-configurators/fields/column/dossier-management-widget-fields-column.component.ts
@@ -35,7 +35,7 @@ import {
   Validators,
 } from '@angular/forms';
 import {TrashCan16} from '@carbon/icons';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 import {CdsThemeService, CurrentCarbonTheme} from '@valtimo/components';
 import {
   CaseWidgetCurrencyDisplayType,
@@ -95,29 +95,53 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
   }
 
   public displayTypeItems: ListItem[] = [
-    {content: CaseWidgetDisplayTypeKey.TEXT, selected: true},
     {
-      content: CaseWidgetDisplayTypeKey.BOOLEAN,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.TEXT}`
+      ),
+      id: CaseWidgetDisplayTypeKey.TEXT,
+      selected: true,
+    },
+    {
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.BOOLEAN}`
+      ),
+      id: CaseWidgetDisplayTypeKey.BOOLEAN,
       selected: false,
     },
     {
-      content: CaseWidgetDisplayTypeKey.CURRENCY,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.CURRENCY}`
+      ),
+      id: CaseWidgetDisplayTypeKey.CURRENCY,
       selected: false,
     },
     {
-      content: CaseWidgetDisplayTypeKey.DATE,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.DATE}`
+      ),
+      id: CaseWidgetDisplayTypeKey.DATE,
       selected: false,
     },
     {
-      content: CaseWidgetDisplayTypeKey.ENUM,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.ENUM}`
+      ),
+      id: CaseWidgetDisplayTypeKey.ENUM,
       selected: false,
     },
     {
-      content: CaseWidgetDisplayTypeKey.NUMBER,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.NUMBER}`
+      ),
+      id: CaseWidgetDisplayTypeKey.NUMBER,
       selected: false,
     },
     {
-      content: CaseWidgetDisplayTypeKey.PERCENT,
+      content: this.translateService.instant(
+        `widgetTabManagement.content.displayType.${CaseWidgetDisplayTypeKey.PERCENT}`
+      ),
+      id: CaseWidgetDisplayTypeKey.PERCENT,
       selected: false,
     },
   ];
@@ -129,7 +153,7 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
 
     return this.displayTypeItems.map((item: ListItem) => ({
       ...item,
-      selected: typeControlValue.content === item.content && typeControlValue.selected,
+      selected: typeControlValue.id === item.id && typeControlValue.selected,
     }));
   }
 
@@ -144,7 +168,7 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
     private readonly cdr: ChangeDetectorRef,
     private readonly fb: FormBuilder,
     private readonly iconService: IconService,
-    private readonly widgetWizardService: WidgetWizardService
+    private readonly translateService: TranslateService
   ) {
     this.iconService.register(TrashCan16);
   }
@@ -188,7 +212,7 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
 
     extraControlKeys.forEach((controlKey: string) => formRow.removeControl(controlKey));
 
-    switch (event.item.content) {
+    switch (event.item.id) {
       case CaseWidgetDisplayTypeKey.BOOLEAN:
         break;
       case CaseWidgetDisplayTypeKey.CURRENCY:
@@ -239,7 +263,12 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
     return this.fb.group({
       type: this.fb.control<ListItem>(
         {
-          content: row.displayProperties?.type ?? CaseWidgetDisplayTypeKey.TEXT,
+          content: this.translateService.instant(
+            this.translateService.instant(
+              `widgetTabManagement.content.displayType.${row.displayProperties?.type ?? CaseWidgetDisplayTypeKey.TEXT}`
+            )
+          ),
+          id: row.displayProperties?.type ?? CaseWidgetDisplayTypeKey.TEXT,
           selected: true,
         },
         Validators.required
@@ -304,11 +333,11 @@ export class DossierManagementWidgetFieldsColumnComponent implements OnInit, OnD
           key: row.title.replace(/\W+/g, '-').replace(/\-$/, '').toLowerCase(),
           title: row.title,
           value: row.content,
-          ...(!!row?.type.content &&
-            row?.type.content !== CaseWidgetDisplayTypeKey.TEXT && {
+          ...(!!row?.type.id &&
+            row?.type.id !== CaseWidgetDisplayTypeKey.TEXT && {
               displayProperties: {
-                type: row.type.content,
-                ...(!!row?.currencyCode && {currentCode: row.currencyCode}),
+                type: row.type.id,
+                ...(!!row?.currencyCode && {currencyCode: row.currencyCode}),
                 ...(!!row?.display && {display: row.display}),
                 ...(!!row?.digitsInfo && {digitsInfo: row.digitsInfo}),
                 ...(!!row?.format && {format: row.format}),
