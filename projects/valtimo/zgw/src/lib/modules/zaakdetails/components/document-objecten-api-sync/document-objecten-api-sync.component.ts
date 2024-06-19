d0c08885c2e186380947fb9eff8497ea8539e1a7
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2024-06-19 16:45:17 +0200

    #101296 Zaakdetail sync through plugins

---
+++ projects/valtimo/zgw/src/lib/modules/zaakdetails/components/document-objecten-api-sync/document-objecten-api-sync.component.ts
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {
+  CARBON_THEME,
+  FormModule,
+  InputModule,
+  SelectItem,
+  SelectModule,
+  SpinnerModule,
+  ValtimoCdsModalDirectiveModule,
+} from '@valtimo/components';
+import {TranslateModule} from '@ngx-translate/core';
+import {BehaviorSubject, map, Observable, Subscription, switchMap, tap} from 'rxjs';
+import {CommonModule} from '@angular/common';
+import {ButtonModule, CheckboxModule, ModalModule} from 'carbon-components-angular';
+import {DocumentObjectenApiSyncService} from '../../services';
+import {DocumentObjectenApiSync} from '../../models';
+import {DocumentDefinition} from '@valtimo/document';
+
+@Component({
+  selector: 'valtimo-document-objecten-api-sync',
+  templateUrl: './document-objecten-api-sync.component.html',
+  styleUrls: ['./document-objecten-api-sync.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    SpinnerModule,
+    FormModule,
+    InputModule,
+    SelectModule,
+    ValtimoCdsModalDirectiveModule,
+    CheckboxModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    ModalModule,
+  ],
+})
+export class DocumentObjectenApiSyncComponent implements OnInit, OnDestroy {
+  readonly loading$ = new BehaviorSubject<boolean>(true);
+  readonly documentDefinitionName$: Observable<string> = this.route.params.pipe(
+    map(params => params.name || '')
+  );
+  private readonly documentDefinition$: Observable<DocumentDefinition> =
+    this.documentDefinitionName$.pipe(
+      switchMap(documentDefinitionName =>
+        this.documentObjectenApiSyncService.getDocumentDefinition(documentDefinitionName)
+      )
+    );
+  readonly documentObjectenApiSync$ = new BehaviorSubject<DocumentObjectenApiSync>(null);
+  readonly objectManagementConfigurationItems$: Observable<Array<SelectItem>> =
+    this.documentObjectenApiSyncService.getObjectManagementConfigurations().pipe(
+      map(results =>
+        results?.map(configuration => ({
+          id: configuration.id,
+          text: configuration.title,
+        }))
+      )
+    );
+  readonly modalShowing$ = new BehaviorSubject<boolean>(false);
+  public readonly formGroup = new FormGroup({
+    objectManagementConfigurationId: new FormControl('', Validators.required),
+    enabled: new FormControl(true),
+  });
+
+  public get objectManagementConfigurationId(): AbstractControl<string> {
+    return this.formGroup.get('objectManagementConfigurationId');
+  }
+
+  public get enabled(): AbstractControl<boolean> {
+    return this.formGroup.get('enabled');
+  }
+
+  public readonly valid$ = new BehaviorSubject<boolean>(false);
+  private _subscriptions = new Subscription();
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly documentObjectenApiSyncService: DocumentObjectenApiSyncService
+  ) {}
+
+  public ngOnInit(): void {
+    this.loadDocumentenObjectenApiSync();
+  }
+
+  public ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
+  }
+
+  public loadDocumentenObjectenApiSync(): void {
+    this._subscriptions.add(
+      this.documentDefinition$
+        .pipe(
+          switchMap(documentDefinition =>
+            this.documentObjectenApiSyncService.getDocumentObjectenApiSync(
+              documentDefinition.id.name,
+              documentDefinition.id.version
+            )
+          )
+        )
+        .subscribe(documentObjectenApiSync => {
+          this.loading$.next(false);
+          this.configSelected(documentObjectenApiSync?.objectManagementConfigurationId);
+          this.enabled.patchValue(documentObjectenApiSync?.enabled);
+          this.documentObjectenApiSync$.next(documentObjectenApiSync);
+        })
+    );
+  }
+
+  public remove(): void {
+    this._subscriptions.add(
+      this.documentDefinition$
+        .pipe(
+          switchMap(documentDefinition =>
+            this.documentObjectenApiSyncService.deleteDocumentObjectenApiSync(
+              documentDefinition.id.name,
+              documentDefinition.id.version
+            )
+          ),
+          tap(() => {
+            this.documentObjectenApiSync$.next(null);
+          })
+        )
+        .subscribe()
+    );
+  }
+
+  public submit(): void {
+    const formValues = this.formGroup.getRawValue();
+    this.documentDefinition$
+      .pipe(
+        switchMap(documentDefinition =>
+          this.documentObjectenApiSyncService.updateDocumentObjectenApiSync(
+            documentDefinition.id.name,
+            documentDefinition.id.version,
+            {
+              objectManagementConfigurationId: formValues.objectManagementConfigurationId,
+              enabled: formValues.enabled,
+            }
+          )
+        )
+      )
+      .subscribe(() => {
+        this.loadDocumentenObjectenApiSync();
+        this.hideModal();
+      });
+  }
+
+  public onModalClose(): void {
+    this.hideModal();
+  }
+
+  public showModal(): void {
+    this.modalShowing$.next(true);
+  }
+
+  public configSelected(selectedId: string): void {
+    if (!selectedId) {
+      this.objectManagementConfigurationId.patchValue('');
+    } else {
+      this.objectManagementConfigurationId.patchValue(selectedId);
+    }
+  }
+
+  private hideModal(): void {
+    this.modalShowing$.next(false);
+  }
+
+  protected readonly CARBON_THEME = CARBON_THEME;
+}
