2f24ba8880f1bc0c1876d965632cbb0f9959eb82
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2024-06-21 09:51:12 +0200

    #101296 Zaakdetail sync through plugins (#1070)
    * #101296 Zaakdetail sync through plugins
    
    * PR comments
    
    * PR comments

--- projects/valtimo/zgw/src/lib/modules/zaakdetails/services/index.ts
+++ projects/valtimo/zgw/src/lib/modules/zaakdetails/services/index.ts
@@ -14,9 +14,4 @@
  * limitations under the License.
  */
 
-export * from './documenten-api';
-export * from './objecten-api';
-export * from './contactmomenten';
-export * from './haalcentraal-brp';
-export * from './zaakdetails';
-export * from './zaken-api';
+export * from './document-objecten-api-sync.service';
