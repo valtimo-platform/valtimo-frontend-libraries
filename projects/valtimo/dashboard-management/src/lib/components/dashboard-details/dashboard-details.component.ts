1a771f4b3ec100bad272483ab06231eac7f7a32f
Maxim Buur <maxim.buur@ritense.com>
2024-07-09 11:16:33 +0200

    reformat

--- projects/valtimo/dashboard-management/src/lib/components/dashboard-details/dashboard-details.component.ts
+++ projects/valtimo/dashboard-management/src/lib/components/dashboard-details/dashboard-details.component.ts
@@ -115,6 +115,7 @@ export class DashboardDetailsComponent implements AfterViewInit {
     new BehaviorSubject<DashboardWidgetConfiguration | null>(null);
 
   public readonly compactMode$ = this.pageHeaderService.compactMode$;
+
   constructor(
     private readonly dashboardManagementService: DashboardManagementService,
     private readonly datePipe: DatePipe,
