1d4a5d9d403d52eb6ae0df8a67816c15569952e8
teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
2024-07-10 13:32:39 +0300

    Addressed Task search fields admin feedback (#1118)

--- projects/valtimo/dossier-management/src/lib/components/dossier-management-search-fields/dossier-management-search-fields.component.ts
+++ projects/valtimo/dossier-management/src/lib/components/dossier-management-search-fields/dossier-management-search-fields.component.ts
@@ -289,16 +289,14 @@ export class DossierManagementSearchFieldsComponent implements OnInit, OnDestroy
       return this.documentService
         .getDropdownData(formData.dropdownDataProvider, documentDefinitionName, formData.key)
         .pipe(
-          map(dropdownData => {
-            if (dropdownData) {
-              return Object.keys(dropdownData).map(dropdownFieldKey => ({
-                key: dropdownFieldKey,
-                value: dropdownData[dropdownFieldKey],
-              }));
-            }
-
-            return [];
-          })
+          map(dropdownData =>
+            dropdownData
+              ? Object.keys(dropdownData).map(dropdownFieldKey => ({
+                  key: dropdownFieldKey,
+                  value: dropdownData[dropdownFieldKey],
+                }))
+              : []
+          )
         );
     })
   );
