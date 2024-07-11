b72b07af04a99d285346e2bd6470fb754c17b33c
Rick Veenstra <rick.veenstra@amsterdam.nl>
2024-07-11 14:31:35 +0200

    feature document upload in bestandsdelen
    Deze feature branch hoort bij de gelijknamige feature branch voor valtimo-backend-libraries

---
+++ projects/valtimo/plugin/src/lib/plugins/documenten-api/components/store-uploaded-document-in-parts/store-uploaded-document-in parts-configuration.component.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {FunctionConfigurationComponent} from '../../../../models';
+import {Observable, Subscription} from 'rxjs';
+
+@Component({
+  selector: 'valtimo-store-uploaded-document-in-parts-configuration',
+  templateUrl: './store-uploaded-document-in-parts-configuration.component.html',
+  styleUrls: ['./store-uploaded-document-in parts-configuration.component.scss'],
+})
+export class StoreUploadedDocumentInPartsConfigurationComponent
+  implements FunctionConfigurationComponent, OnInit, OnDestroy
+{
+  @Input() save$: Observable<void>;
+  @Input() disabled$: Observable<boolean>;
+  @Input() pluginId: string;
+  @Input() prefillConfiguration$: Observable<any>;
+  @Output() valid: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() configuration: EventEmitter<any> = new EventEmitter<any>();
+
+  private saveSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.openSaveSubscription();
+    this.emitValid();
+  }
+
+  ngOnDestroy(): void {
+    this.saveSubscription?.unsubscribe();
+  }
+
+  private emitValid(): void {
+    this.valid.emit(true);
+  }
+
+  private openSaveSubscription(): void {
+    this.saveSubscription = this.save$?.subscribe(() => {
+      this.configuration.emit({});
+    });
+  }
+}
--- projects/valtimo/plugin/src/public-api.ts
+++ projects/valtimo/plugin/src/public-api.ts
@@ -47,6 +47,7 @@ export * from './lib/plugins/documenten-api/components/store-temp-document/store
 export * from './lib/plugins/documenten-api/components/documenten-api-configuration/documenten-api-configuration.component';
 export * from './lib/plugins/documenten-api/components/store-uploaded-document/store-uploaded-document-configuration.component';
 export * from './lib/plugins/documenten-api/components/download-document/download-document-configuration.component';
+export * from './lib/plugins/documenten-api/components/store-uploaded-document-in-parts/store-uploaded-document-in parts-configuration.component';
 /* objecttypen api plugin */
 export * from './lib/plugins/objecttypen-api/objecttypen-api-plugin-module';
 export * from './lib/plugins/objecttypen-api/objecttypen-api-plugin.specification';
