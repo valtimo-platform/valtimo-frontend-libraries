0ad87cf45fe954f32fe0e9200860b8a3a6371735
teodor-ritense <teodor.dragoi@ritense.com>
2024-09-26 10:21:31 +0300

    Add copyright

--- projects/valtimo/logging/src/lib/components/logging-list/logging-list.component.ts
+++ projects/valtimo/logging/src/lib/components/logging-list/logging-list.component.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
