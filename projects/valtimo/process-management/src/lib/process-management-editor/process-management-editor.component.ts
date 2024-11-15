0a1ccfbdab70cc0419583cdc47a8fee861e46f3e
Maxim Buur <maxim.buur@ritense.com>
2024-11-15 14:06:35 +0100

    work on editor

--- projects/valtimo/process-management/src/lib/process-management-editor/process-management-editor.component.ts
+++ projects/valtimo/process-management/src/lib/process-management-editor/process-management-editor.component.ts
@@ -16,23 +16,38 @@
 
 import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FitPageDirectiveModule, PageTitleService} from '@valtimo/components';
+import {
+  FitPageDirectiveModule,
+  PageTitleService,
+  RenderInPageHeaderDirectiveModule,
+} from '@valtimo/components';
 import {ActivatedRoute} from '@angular/router';
-import {BehaviorSubject, filter, map, switchMap, take, tap} from 'rxjs';
+import {BehaviorSubject, combineLatest, filter, map, Observable, switchMap, take, tap} from 'rxjs';
 import {ProcessDefinition, ProcessService} from '@valtimo/process';
-import {LoadingModule} from 'carbon-components-angular';
+import {DropdownModule, ListItem, LoadingModule, SelectModule} from 'carbon-components-angular';
 import Modeler from 'bpmn-js/lib/Modeler';
 import BpmnViewer from 'bpmn-js';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'valtimo-process-management-editor',
   templateUrl: './process-management-editor.component.html',
   styleUrls: ['./process-management-editor.component.scss'],
   standalone: true,
-  imports: [CommonModule, FitPageDirectiveModule, LoadingModule],
+  imports: [
+    CommonModule,
+    FitPageDirectiveModule,
+    LoadingModule,
+    RenderInPageHeaderDirectiveModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    SelectModule,
+  ],
 })
 export class ProcessManagementEditorComponent implements AfterViewInit, OnDestroy {
   @ViewChild('modeler', {static: false}) modelerElementRef!: ElementRef;
+  @ViewChild('viewer', {static: false}) viewerElementRef!: ElementRef;
 
   public readonly loading$ = new BehaviorSubject<boolean>(true);
 
@@ -42,6 +57,9 @@ export class ProcessManagementEditorComponent implements AfterViewInit, OnDestro
 
   private _bpmnModeler!: Modeler;
   private _bpmnViewer!: BpmnViewer;
+
+  public isReadOnlyProcess$ = new BehaviorSubject<boolean>(false);
+  public isSystemProcess$ = new BehaviorSubject<boolean>(false);
 
   public readonly selectedProcessDefinitionXml$ = this._selectionProcessDefinition$.pipe(
     filter(selectedProcessDefinition => !!selectedProcessDefinition?.id),
@@ -54,6 +72,9 @@ export class ProcessManagementEditorComponent implements AfterViewInit, OnDestro
     ),
     tap(result => {
       this._bpmnModeler?.importXML(result.bpmn20Xml);
+      this._bpmnViewer?.importXML(result.bpmn20Xml);
+      this.isReadOnlyProcess$.next(result.readOnly);
+      this.isSystemProcess$.next(result.systemProcess);
       this.loading$.next(false);
     })
   );
@@ -70,17 +91,36 @@ export class ProcessManagementEditorComponent implements AfterViewInit, OnDestro
     tap(processDefinitions => this.setSelectedProcessDefinitionOnInit(processDefinitions))
   );
 
+  public readonly processDefinitionVersionsListItems$: Observable<ListItem[]> = combineLatest([
+    this.processDefinitionVersions$,
+    this._selectionProcessDefinition$,
+    this.translateService.stream('key'),
+  ]).pipe(
+    map(([processDefinitionVersions, selectionProcessDefinition]) =>
+      processDefinitionVersions.map(processDefinitionVersion => ({
+        id: processDefinitionVersion.version,
+        content: `${this.translateService.instant('processManagementEditor.version')}${processDefinitionVersion.version}`,
+        selected: selectionProcessDefinition.version === processDefinitionVersion.version,
+      }))
+    )
+  );
+
   constructor(
     private readonly route: ActivatedRoute,
     private readonly processService: ProcessService,
-    private readonly pageTitleService: PageTitleService
+    private readonly pageTitleService: PageTitleService,
+    private readonly translateService: TranslateService
   ) {}
 
   public ngAfterViewInit(): void {
     this.initModeler();
+    this.initViewer();
   }
 
-  public ngOnDestroy(): void {}
+  public ngOnDestroy(): void {
+    this._bpmnModeler?.destroy();
+    this._bpmnViewer?.destroy();
+  }
 
   private setSelectedProcessDefinitionOnInit(processDefinitions: ProcessDefinition[]): void {
     this._selectionProcessDefinition$.pipe(take(1)).subscribe(selectedProcessDefinition => {
@@ -95,5 +135,10 @@ export class ProcessManagementEditorComponent implements AfterViewInit, OnDestro
   private initModeler(): void {
     this._bpmnModeler = new Modeler();
     this._bpmnModeler?.attachTo(this.modelerElementRef.nativeElement);
+  }
+
+  private initViewer(): void {
+    this._bpmnViewer = new BpmnViewer();
+    this._bpmnViewer?.attachTo(this.viewerElementRef.nativeElement);
   }
 }
