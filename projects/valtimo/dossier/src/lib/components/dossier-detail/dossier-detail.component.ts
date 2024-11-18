5825764de2684967edcb1f7101c8cec3679b7595
Sofia Ivars <sofia.ivars@ritense.com>
2024-11-18 09:22:51 +0100

    Add no access page for case details

--- projects/valtimo/dossier/src/lib/components/dossier-detail/dossier-detail.component.ts
+++ projects/valtimo/dossier/src/lib/components/dossier-detail/dossier-detail.component.ts
@@ -78,6 +78,7 @@ import {TabImpl, TabLoaderImpl} from '../../models';
 import {
   CAN_ASSIGN_CASE_PERMISSION,
   CAN_CLAIM_CASE_PERMISSION,
+  CAN_VIEW_CASE_PERMISSION,
   DOSSIER_DETAIL_PERMISSION_RESOURCE,
 } from '../../permissions';
 import {DossierDetailLayoutService, DossierService, DossierTabService} from '../../services';
@@ -102,6 +103,7 @@ export class DossierDetailComponent
   @ViewChild('tabContentContainer')
   private readonly _tabContentContainer!: ElementRef<HTMLDivElement>;
 
+  public hasAccess = CAN_VIEW_CASE_PERMISSION;
   public customDossierHeaderItems: Array<any> = [];
   public document: ValtimoDocument | null = null;
   public documentDefinitionName: string;
@@ -223,6 +225,16 @@ export class DossierDetailComponent
         identifier: params.get('documentId') ?? '',
       })
     )
+  );
+
+  public readonly canView$: Observable<boolean> = this.route.paramMap.pipe(
+    switchMap((params: ParamMap) =>
+      this.permissionService.requestPermission(CAN_VIEW_CASE_PERMISSION, {
+        resource: DOSSIER_DETAIL_PERMISSION_RESOURCE.jsonSchemaDocument,
+        identifier: params.get('documentId') ?? '',
+      })
+    ),
+    tap(result => console.log('Result: ', result))
   );
 
   public readonly loadingTabs$ = new BehaviorSubject<boolean>(true);
