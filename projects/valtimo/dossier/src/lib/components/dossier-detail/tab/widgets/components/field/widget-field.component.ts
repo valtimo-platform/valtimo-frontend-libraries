892855b89627d2acfda8ebeee5c1b529aa41de7d
teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
2024-12-11 14:12:35 +0100

    Bugfix/130515 boolean resolver (#1300)

--- projects/valtimo/dossier/src/lib/components/dossier-detail/tab/widgets/components/field/widget-field.component.ts
+++ projects/valtimo/dossier/src/lib/components/dossier-detail/tab/widgets/components/field/widget-field.component.ts
@@ -27,11 +27,7 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
-<<<<<<< HEAD
 import {CarbonListModule, EllipsisPipe, ViewContentService, ViewType} from '@valtimo/components';
-=======
-import {CarbonListModule, EllipsisPipe, ViewContentService} from '@valtimo/components';
->>>>>>> 0a981381 (Story/129861 widget field ellipsis (#1296))
 import {InputModule} from 'carbon-components-angular';
 import {BehaviorSubject, combineLatest, map, Observable} from 'rxjs';
 import {CaseWidgetTextDisplayType, FieldsCaseWidget} from '../../../../../../models';
@@ -82,13 +78,10 @@ export class WidgetFieldComponent implements AfterViewInit, OnDestroy {
                     ellipsisCharacterLimit:
                       (property.displayProperties as CaseWidgetTextDisplayType)
                         ?.ellipsisCharacterLimit ?? null,
-                    value:
-                      widgetData[property.key] !== null && widgetData[property.key] !== undefined
-                        ? this.viewContentService.get(widgetData[property.key], {
-                            ...property.displayProperties,
-                            viewType: property.displayProperties?.type ?? 'text',
-                          })
-                        : '-',
+                    value: this.viewContentService.get(widgetData[property.key], {
+                      ...property.displayProperties,
+                      viewType: property.displayProperties?.type ?? ViewType.TEXT,
+                    }),
                   },
                 ]
               : []),
