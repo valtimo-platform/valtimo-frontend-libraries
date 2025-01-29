5deb42aef5c1103854a4e034293ed9cc25d40209
Maarten <maarten.van.toor@ritense.com>
2025-01-29 15:48:15 +0100

    Option to configure an external create case form per case (#1334)
    * Add fix for width chaning in plugin config (#1329)
    
    * fix files being duplicated on upload (#1330)
    
    * WIP: external case create form
    
    * Story/fvm fixes (#1332)
    
    * fix fvm not showing errors, translations, fix missing updateStartForm handle
    
    * feature/fvm-bugfixes fix error message not showing
    
    * fix breadcrumbs endpoint call (#1333)
    
    * added option to specify an external case create form on dossier management configuration tab
    
    * added additional translations
    
    * improved URL validation
    
    * removed unused method
    
    * added translations and toastr
    
    * checking case definition setting if an external create case form is configured, if so open the form in a new window instead of the regular start case modal
    
    * updated regex due to a code scanning alert was triggered
    
    ---------
    
    Co-authored-by: teodor-ritense <135605531+teodor-ritense@users.noreply.github.com>
    Co-authored-by: mbritense <79840403+mbritense@users.noreply.github.com>
    Co-authored-by: floris-thijssen-ritense <floris.thijssen@ritense.com>

---
+++ projects/valtimo/dossier-management/src/lib/components/dossier-management-external-create-case-form/dossier-management-external-create-case-form.component.ts
@@ -0,0 +1,140 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BehaviorSubject, filter, map, Observable, Subscription} from 'rxjs';
+import {CaseSettings, DocumentService} from '@valtimo/document';
+import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NGXLogger} from 'ngx-logger';
+import {ToastrService} from 'ngx-toastr';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'valtimo-dossier-management-external-create-case-form',
+  templateUrl: './dossier-management-external-create-case-form.component.html',
+})
+export class DossierManagementExternalCreateCaseFormComponent implements OnInit, OnDestroy {
+  public form!: FormGroup;
+
+  readonly documentDefinitionName$: Observable<string> = this.route.params.pipe(
+    map(params => params?.name),
+    filter(docDefName => !!docDefName)
+  );
+
+  readonly caseSettings$: BehaviorSubject<CaseSettings> = new BehaviorSubject(null);
+
+  private readonly urlPattern = new RegExp(
+    '^(https?:\\/\\/)(([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}|\\d{1,3}(\\.\\d{1,3}){3})(:\\d+)?(\\/\\S*)?(\\?\\S*)?(#\\S*)?$'
+  );
+
+  private _subscriptions = new Subscription();
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly documentService: DocumentService,
+    private readonly fb: FormBuilder,
+    private readonly translateService: TranslateService,
+    private readonly toastrService: ToastrService,
+    private readonly logger: NGXLogger,
+  ) { }
+
+  ngOnInit(): void {
+    this.logger.debug('External Case Create Form - onInit');
+
+    this.form = this.fb.group({
+      hasExternalForm: [false], // Toggle is off by default
+      externalFormUrl: [ {value: '', disabled: true}, [
+        Validators.required,
+        Validators.pattern(this.urlPattern),
+        Validators.maxLength(512)
+      ]],
+    });
+
+    // Subscribe to the toggle field value changes
+    this._subscriptions.add(
+      this.hasExternalForm?.valueChanges.subscribe(isEnabled => {
+        if (isEnabled) {
+          this.externalFormUrl.enable();
+        } else {
+          this.externalFormUrl.disable();
+          this.externalFormUrl.reset(); // Clear the URL field when disabled
+        }
+      })
+    );
+
+    this._subscriptions.add(
+      this.documentDefinitionName$.subscribe(documentDefinitionName => {
+        this.logger.debug(
+          'Fetching case definition settings for documentDefinitionName',
+          documentDefinitionName
+        );
+        this.documentService
+          .getCaseSettingsForManagement(documentDefinitionName)
+          .subscribe(caseSettings => {
+            this.logger.debug('Fetched case definition settings', caseSettings);
+            this.caseSettings$.next(caseSettings);
+          });
+      })
+    );
+
+    this._subscriptions.add(
+      this.caseSettings$.subscribe(caseSettings => {
+        if (caseSettings) {
+          this.logger.debug('Applying case definition settings to form', caseSettings);
+          this.form.setValue({
+            hasExternalForm: caseSettings.hasExternalCreateCaseForm,
+            externalFormUrl: caseSettings.externalCreateCaseFormUrl,
+          });
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.logger.debug('External Case Create Form - onDestroy');
+    // Clean up subscriptions when the component is destroyed
+    this._subscriptions.unsubscribe();
+  }
+
+  public get hasExternalForm() {
+    return this.form.get('hasExternalForm');
+  }
+
+  public get externalFormUrl() {
+    return this.form.get('externalFormUrl');
+  }
+
+  public canSubmit(): boolean {
+    return this.form.valid;
+  }
+
+  public onSubmit(): void {
+    if (this.canSubmit()) {
+      this.logger.debug('Submitted case definition settings form with values:', this.form.value);
+      const caseSettings = this.caseSettings$.getValue();
+      this.updateCaseSettings(caseSettings.name, {
+        hasExternalCreateCaseForm: this.hasExternalForm.value,
+        externalCreateCaseFormUrl: (typeof this.externalFormUrl.value === 'string') ?
+          this.externalFormUrl.value.trim() : this.externalFormUrl.value
+      });
+    }
+  }
+
+  private updateCaseSettings(documentDefinitionName: string, caseSettings: CaseSettings): void {
+    this.logger.debug('Updating case definition settings', documentDefinitionName, caseSettings);
+    this.documentService
+      .patchCaseSettingsForManagement(documentDefinitionName, caseSettings)
+      .subscribe({
+        next: result => {
+          this.logger.debug('Updated case definition settings', result);
+          this.caseSettings$.next(result);
+        },
+        error: e => {
+          this.logger.error('An error occurred while updating case definition settings', e);
+          this.toastrService.error(this.translateService.instant('dossierManagement.externalCreateCaseForm.notification.error'))
+        },
+        complete: () => {
+          this.logger.debug('Finished updating case definition settings');
+          this.toastrService.success(this.translateService.instant('dossierManagement.externalCreateCaseForm.notification.success'));
+        },
+      });
+  }
+}
--- projects/valtimo/dossier/src/lib/components/dossier-list-actions/dossier-list-actions.component.ts
+++ projects/valtimo/dossier/src/lib/components/dossier-list-actions/dossier-list-actions.component.ts
@@ -17,11 +17,12 @@ import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angula
 import {Router} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 import {CARBON_CONSTANTS} from '@valtimo/components';
-import {DocumentService, ProcessDocumentDefinition} from '@valtimo/document';
+import {CaseSettings, DocumentService, ProcessDocumentDefinition} from '@valtimo/document';
 import {NotificationService} from 'carbon-components-angular';
 import {BehaviorSubject, combineLatest, map, Observable, of, switchMap} from 'rxjs';
 import {DossierListService} from '../../services';
 import {DossierProcessStartModalComponent} from '../dossier-process-start-modal/dossier-process-start-modal.component';
+import {NGXLogger} from 'ngx-logger';
 
 declare const $;
 
@@ -63,6 +64,8 @@ export class DossierListActionsComponent implements OnInit {
     })
   );
 
+  readonly caseSettings$: BehaviorSubject<CaseSettings> = new BehaviorSubject(null);
+
   private selectedProcessDocumentDefinition: ProcessDocumentDefinition | null = null;
   private modalListenerAdded = false;
   private _cachedAssociatedProcessDocumentDefinitions: Array<ProcessDocumentDefinition> = [];
@@ -72,21 +75,41 @@ export class DossierListActionsComponent implements OnInit {
     private readonly listService: DossierListService,
     private readonly notificationService: NotificationService,
     private readonly router: Router,
-    private readonly translateService: TranslateService
+    private readonly translateService: TranslateService,
+    private readonly logger: NGXLogger,
   ) {}
 
   public ngOnInit(): void {
     this.modalListenerAdded = false;
+
+    this.listService.documentDefinitionName$.subscribe(documentDefinitionName => {
+      this.logger.debug(
+        'Fetching case definition settings for documentDefinitionName', documentDefinitionName
+      );
+      this.documentService
+        .getCaseSettings(documentDefinitionName)
+        .subscribe(caseSettings => {
+          this.logger.debug('Fetched case definition settings', caseSettings);
+          this.caseSettings$.next(caseSettings);
+        });
+    });
   }
 
   public startDossier(): void {
-    const associatedProcessDocumentDefinitions = this._cachedAssociatedProcessDocumentDefinitions;
-
-    if (associatedProcessDocumentDefinitions.length > 1) {
-      $('#startProcess').modal('show');
+    const caseSettings = this.caseSettings$.getValue();
+    this.logger.debug('Has external Create Case form?', caseSettings.hasExternalCreateCaseForm)
+    if (caseSettings.hasExternalCreateCaseForm) {
+      this.logger.debug('External Create Case form URL:', caseSettings.externalCreateCaseFormUrl)
+      window.open(caseSettings.externalCreateCaseFormUrl, '_blank');
     } else {
-      this.selectedProcessDocumentDefinition = associatedProcessDocumentDefinitions[0];
-      this.showStartProcessModal();
+      const associatedProcessDocumentDefinitions = this._cachedAssociatedProcessDocumentDefinitions;
+
+      if (associatedProcessDocumentDefinitions.length > 1) {
+        $('#startProcess').modal('show');
+      } else {
+        this.selectedProcessDocumentDefinition = associatedProcessDocumentDefinitions[0];
+        this.showStartProcessModal();
+      }
     }
   }
 
