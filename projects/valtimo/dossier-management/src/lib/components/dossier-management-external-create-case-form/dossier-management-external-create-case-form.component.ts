2bba6e8630b17dfdf055998233a44b6523b8959f
Maarten van Toor <maarten.van.toor@ritense.com>
2025-01-29 14:27:01 +0100

    updated regex due to a code scanning alert was triggered

--- projects/valtimo/dossier-management/src/lib/components/dossier-management-external-create-case-form/dossier-management-external-create-case-form.component.ts
+++ projects/valtimo/dossier-management/src/lib/components/dossier-management-external-create-case-form/dossier-management-external-create-case-form.component.ts
@@ -22,13 +22,8 @@ export class DossierManagementExternalCreateCaseFormComponent implements OnInit,
   readonly caseSettings$: BehaviorSubject<CaseSettings> = new BehaviorSubject(null);
 
   private readonly urlPattern = new RegExp(
-    '^(https?:\\/\\/)?' + // validate protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
-    '(\\#[-a-z\\d_]*)?$','i'
-  ); // validate fragment locator
+    '^(https?:\\/\\/)(([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}|\\d{1,3}(\\.\\d{1,3}){3})(:\\d+)?(\\/\\S*)?(\\?\\S*)?(#\\S*)?$'
+  );
 
   private _subscriptions = new Subscription();
 
