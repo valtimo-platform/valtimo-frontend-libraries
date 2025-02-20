afcfef2bb5fa97814561e15475cb9806fa6b8b7d
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-02-20 15:41:08 +0100

    Fix permission

--- projects/valtimo/zgw/src/lib/modules/documenten-api/components/documenten-api-documents/documenten-api-documents.component.ts
+++ projects/valtimo/zgw/src/lib/modules/documenten-api/components/documenten-api-documents/documenten-api-documents.component.ts
@@ -522,7 +522,7 @@ export class DossierDetailTabDocumentenApiDocumentsComponent implements OnInit, 
   }
 
   private editDisabled(file: DocumentenApiRelatedFile): boolean {
-    return file.status === 'definitief' || !this.filePermissions[file.fileId]?.canDelete;
+    return file.status === 'definitief' || !this.filePermissions[file.fileId]?.canModify;
   }
 
   private deleteDisabled(file: DocumentenApiRelatedFile): boolean {
