ac04b951d671bb5125ac795c6daa254e708f7059
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2025-02-24 12:23:55 +0100

    doc: Add PBAC permissions to Documenten API documents (#1348)

--- projects/valtimo/resource/src/public-api.ts
+++ projects/valtimo/resource/src/public-api.ts
@@ -24,3 +24,4 @@ export * from './lib/services/open-zaak.service';
 export * from './lib/services/upload-provider.service';
 export * from './lib/services/s3.service';
 export * from './lib/services/download.service';
+export * from './lib/resource-permissions';
