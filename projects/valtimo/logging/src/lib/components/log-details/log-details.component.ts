87972b9487c52a6e19ca97073de0ddfd070a8475
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-03-03 12:21:24 +0100

    remove link

--- projects/valtimo/logging/src/lib/components/log-details/log-details.component.ts
+++ projects/valtimo/logging/src/lib/components/log-details/log-details.component.ts
@@ -41,21 +41,6 @@ export class LogDetailsComponent {
 
   public logEventFormatted: LoggingEvent;
 
-  public readonly packagesToIgnore: string[] = [
-    'com.zaxxer',
-    'io.micrometer',
-    'jakarta',
-    'java',
-    'jdk',
-    'kotlin',
-    'org.apache',
-    'org.hibernate',
-    'org.jboss',
-    'org.keycloak',
-    'org.postgresql',
-    'org.spring',
-    'sun.',
-  ];
   public readonly classesToIgnore: string[] = [
     'LoggingContextKt',
     'LoggableResourceAspect',
@@ -78,82 +63,12 @@ export class LogDetailsComponent {
       c =>
         (s = s.replace(
           new RegExp(`^\tat .+\.${c}\..+`, 'gm'),
-          '<span class="highlight4">$&</span>'
+          '<span class="highlight3">$&</span>'
         ))
     );
-    this.packagesToIgnore.forEach(
-      p => (s = s.replace(new RegExp(`^\tat ${p}.+`, 'gm'), '<span class="highlight4">$&</span>'))
-    );
     s = s.replace(/^\tat com.(ritense|valtimo).+/gm, '<span class="highlight2">$&</span>');
-    s = s.replace(/^\t.+common frames omitted$/gm, '<span class="highlight4">$&</span>');
-    s = s.replace(/com.(ritense|valtimo)[^<\n]+/gm, this.addLinkToValtimoBackendLibraries);
+    s = s.replace(/^\t.+common frames omitted$/gm, '<span class="highlight3">$&</span>');
+    s = s.replace(/^\tat .+/gm, '<span class="highlight3">$&</span>');
     return s;
-  }
-
-  private addLinkToValtimoBackendLibraries(classMatch: string): string {
-    const match = classMatch.match(
-      /(.*(com\.(ritense|valtimo).+?)\.[A-Z].+?\()([^A-Z]*([A-Z][^.]+)\.([a-z]+):?([0-9]+)?)(.+)/
-    );
-    if (!match) {
-      return classMatch;
-    }
-    const [all, start, packge, company, linkText, className, ext, lineNr, end] = match;
-    const module = LogDetailsComponent.getModule(packge, className);
-    const language = ext === 'kt' ? 'kotlin' : ext;
-    const classPath = `${packge.replace(/\./g, '/')}`;
-    const line = lineNr ? `#L${lineNr}` : '';
-    const url = `https://github.com/valtimo-platform/valtimo-backend-libraries/blob/next-minor/${module}/src/main/${language}/${classPath}/${className}.${ext}${line}`;
-    const link = `<a href="${url}" target="_blank">${linkText}</a>`;
-    return `${start}${link}${end}`;
-  }
-
-  private static getModule(packge: string, className: string): string {
-    let module = packge.match(/(ritense|valtimo)\.((valtimo\.)?[a-z_]+)/)[2];
-    switch (module) {
-      case 'processdocument':
-        return 'process-document';
-      case 'valueresolver':
-        return 'value-resolver';
-      case 'formflow':
-        return 'form-flow';
-      case 'valtimo.formflow':
-        return 'form-flow-valtimo';
-      case 'besluitenapi':
-        return 'zgw/besluiten-api';
-      case 'catalogiapi':
-        return 'zgw/catalogi-api';
-      case 'documentenapi':
-        return 'zgw/documenten-api';
-      case 'notificatiesapi':
-        return 'zgw/notificaties-api';
-      case 'objectmanagement':
-        return 'zgw/object-management';
-      case 'objectenapi':
-        return 'zgw/objecten-api';
-      case 'objecttypenapi':
-        return 'zgw/objecttypen-api';
-      case 'portaaltaak':
-        return 'zgw/portaaltaak';
-      case 'verzoek':
-        return 'zgw/verzoek';
-      case 'zaakdetails':
-        return 'zgw/zaakdetails';
-      case 'zakenapi':
-        return 'zgw/zaken-api';
-    }
-    switch (packge) {
-      case 'com.ritense.valtimo.logging':
-      case 'com.ritense.valtimo.service':
-        return 'core';
-      case 'com.ritense.valtimo.processlink':
-        return 'plugin-valtimo';
-    }
-    switch (className) {
-      case 'ResourceStorageDelegate':
-      case 'TemporaryResourceStorageService':
-        return 'resource/temporary-resource-storage';
-    }
-    const s = module.split('.');
-    return s.length == 2 ? s[1] : s[0];
   }
 }
