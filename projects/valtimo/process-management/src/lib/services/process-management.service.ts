e27441f94d9f19d6b7145ffbcea33c8bf57ec093
teodor-ritense <teodor.dragoi@ritense.com>
2025-03-05 16:27:08 +0100

    Update field config for processes

--- projects/valtimo/process-management/src/lib/services/process-management.service.ts
+++ projects/valtimo/process-management/src/lib/services/process-management.service.ts
@@ -1,5 +1,4 @@
 import {HttpClient} from '@angular/common/http';
-<<<<<<< HEAD
 import {Injectable, Signal, signal} from '@angular/core';
 import {BaseApiService, ConfigService} from '@valtimo/config';
 import {BehaviorSubject, combineLatest, filter, Observable, switchMap} from 'rxjs';
@@ -9,18 +8,11 @@ import {
   PROCESS_MANAGEMENT_ENDPOINTS,
   ProcessManagementContext,
 } from '../models';
-=======
-import {Injectable} from '@angular/core';
-import {BaseApiService, ConfigService} from '@valtimo/config';
-import {Observable, tap} from 'rxjs';
-import { CaseProcessInstance } from '../models';
->>>>>>> 189cf259 (Cleanup)
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProcessManagementService extends BaseApiService {
-<<<<<<< HEAD
   private readonly _definitionName$ = new BehaviorSubject<string | null>(null);
   private readonly _versionTag$ = new BehaviorSubject<string | null>(null);
 
@@ -41,10 +33,6 @@ export class ProcessManagementService extends BaseApiService {
   public get context(): Signal<ProcessManagementContext> {
     return this._context.asReadonly();
   }
-=======
-  private _documentDefinitionKey: string;
-  private _versionTag: string;
->>>>>>> 189cf259 (Cleanup)
 
   constructor(
     protected readonly httpClient: HttpClient,
@@ -53,33 +41,15 @@ export class ProcessManagementService extends BaseApiService {
     super(httpClient, configService);
   }
 
-<<<<<<< HEAD
   public setParams(definitionName: string, versionTag: string): void {
     this._definitionName$.next(definitionName);
     this._versionTag$.next(versionTag);
-=======
-  public setParams(documentDefinitionKey: string, versionTag: string): void {
-    this._documentDefinitionKey = documentDefinitionKey;
-    this._versionTag = versionTag;
-  }
-
-  public getProcesses(): Observable<CaseProcessInstance[]> {
-    return this.httpClient.get<CaseProcessInstance[]>(
-      this.getApiUrl(
-        `/management/v1/case-definition/${this._documentDefinitionKey}/version/${this._versionTag}/process-definition`
-      )
-    );
->>>>>>> 189cf259 (Cleanup)
   }
 
   public deleteProcess(processDefinitionId: string): Observable<void> {
     return this.httpClient.delete<void>(
       this.getApiUrl(
-<<<<<<< HEAD
         `${PROCESS_MANAGEMENT_ENDPOINTS[this._context()]}/${this._definitionName$.getValue()}/version/${this._versionTag$.getValue()}/process-definition/${processDefinitionId}`
-=======
-        `/management/v1/case-definition/${this._documentDefinitionKey}/version/${this._versionTag}/process-definition/${processDefinitionId}`
->>>>>>> 189cf259 (Cleanup)
       )
     );
   }
@@ -96,17 +66,12 @@ export class ProcessManagementService extends BaseApiService {
 
     return this.httpClient.post<any>(
       this.getApiUrl(
-<<<<<<< HEAD
         `${PROCESS_MANAGEMENT_ENDPOINTS[this._context()]}/${this._definitionName$.getValue()}/version/${this._versionTag$.getValue()}/process-definition`
-=======
-        `/management/v1/case-definition/${this._documentDefinitionKey}/version/${this._versionTag}/process-definition`
->>>>>>> 189cf259 (Cleanup)
       ),
       formData
     );
   }
 
-<<<<<<< HEAD
   private getProcesses(
     definitionName: string,
     versionTag: string
@@ -118,8 +83,6 @@ export class ProcessManagementService extends BaseApiService {
     );
   }
 
-=======
->>>>>>> 189cf259 (Cleanup)
   private emptyStringToNull<T extends Record<string, any>>(object: T): T {
     if (object && typeof object === 'object') {
       Object.keys(object).forEach(key => {
