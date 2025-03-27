97311d5cf5a12d22094d91ec08b1efcc345e72ca
Sofia Ivars <sofia.ivars@ritense.com>
2025-03-27 13:36:08 +0100

    Remove unnecessary models

--- projects/valtimo/dossier-management/src/lib/components/case-management-general/models/index.ts
+++ projects/valtimo/dossier-management/src/lib/components/case-management-general/models/index.ts
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-export * from './documenten-api-metadata.model';
 export * from './documenten-api-link-process.model';
 export * from './documenten-api-version.model';
