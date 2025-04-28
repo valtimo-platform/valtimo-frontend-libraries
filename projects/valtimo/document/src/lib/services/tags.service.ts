6ff1a752e27c329ffa6e9a27925af5bda7c0c1a7
Maxim Buur <maxim.buur@ritense.com>
2025-04-28 15:42:15 +0200

    fix case tags

--- projects/valtimo/document/src/lib/services/tags.service.ts
+++ projects/valtimo/document/src/lib/services/tags.service.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2015-2024 Ritense BV, the Netherlands.
- *
- * Licensed under EUPL, Version 1.2 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {Injectable} from '@angular/core';
 import {BaseApiService, ConfigService} from '@valtimo/config';
 import {HttpClient} from '@angular/common/http';
@@ -31,49 +15,77 @@ export class CaseTagService extends BaseApiService {
     super(httpClient, configService);
   }
 
-  public getCaseTagsManagement(caseDefinitionName: string): Observable<CaseTag[]> {
+  public getCaseTagsManagement(
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string
+  ): Observable<CaseTag[]> {
     return this.httpClient.get<CaseTag[]>(
-      this.getApiUrl(`/management/v1/case-definition/${caseDefinitionName}/case-tag`)
+      this.getApiUrl(
+        `/management/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag`
+      )
     );
   }
 
-  public getCaseTags(caseDefinitionName: string): Observable<CaseTag[]> {
+  public getCaseTags(
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string
+  ): Observable<CaseTag[]> {
     return this.httpClient.get<CaseTag[]>(
-      this.getApiUrl(`/v1/case-definition/${caseDefinitionName}/case-tag`)
+      this.getApiUrl(
+        `/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag`
+      )
     );
   }
 
-  public saveCaseTag(caseDefinitionName: string, tag: CaseTag): Observable<CaseTag> {
+  public saveCaseTag(
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string,
+    tag: CaseTag
+  ): Observable<CaseTag> {
     return this.httpClient.post<CaseTag>(
-      this.getApiUrl(`/management/v1/case-definition/${caseDefinitionName}/case-tag`),
+      this.getApiUrl(
+        `/management/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag`
+      ),
       tag
     );
   }
 
   public updateCaseTag(
-    caseDefinitionName: string,
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string,
     currentTag: string,
     updatedTag: CaseTag
   ): Observable<CaseTag> {
     return this.httpClient.put<CaseTag>(
-      this.getApiUrl(`/management/v1/case-definition/${caseDefinitionName}/case-tag/${currentTag}`),
+      this.getApiUrl(
+        `/management/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag/${currentTag}`
+      ),
       updatedTag
     );
   }
 
-  public deleteCaseTag(caseDefinitionName: string, tag: string): Observable<void> {
+  public deleteCaseTag(
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string,
+    tag: string
+  ): Observable<void> {
     return this.httpClient.delete<void>(
-      this.getApiUrl(`/management/v1/case-definition/${caseDefinitionName}/case-tag/${tag}`)
+      this.getApiUrl(
+        `/management/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag/${tag}`
+      )
     );
   }
 
   public updateCaseTags(
-    caseDefinitionName: string,
-    reorderedStatus: CaseTag[]
+    caseDefinitionKey: string,
+    caseDefinitionVersionTag: string,
+    reorderedTags: CaseTag[]
   ): Observable<CaseTag[]> {
     return this.httpClient.put<CaseTag[]>(
-      this.getApiUrl(`/management/v1/case-definition/${caseDefinitionName}/case-tag`),
-      reorderedStatus
+      this.getApiUrl(
+        `/management/v1/case-definition/${caseDefinitionKey}/version/${caseDefinitionVersionTag}/case-tag`
+      ),
+      reorderedTags
     );
   }
 }
