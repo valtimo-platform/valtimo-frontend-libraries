0e1418b6f526f1ab473d5947f61c2dfb8e1298aa
Sofia Ivars <sofia.ivars@ritense.com>
2025-05-22 10:05:41 +0200

    Hide delete draft button when the version is globally active

--- projects/valtimo/case-management/src/lib/components/case-management-deployment/case-management-deployment.component.ts
+++ projects/valtimo/case-management/src/lib/components/case-management-deployment/case-management-deployment.component.ts
@@ -92,6 +92,22 @@ export class CaseManagementDeploymentComponent implements OnInit, AfterViewInit 
     )
   );
 
+  public readonly isGloballyActive$: Observable<boolean> = combineLatest([
+    this.globalActiveCase$,
+    this.caseDefinitionKey$,
+    this.caseDefinitionVersionTag$,
+  ]).pipe(
+    map(([globalActiveCase, caseDefinitionKey, caseDefinitionVersionTag]) => {
+      if (
+        globalActiveCase.caseDefinitionKey === caseDefinitionKey &&
+        globalActiveCase.caseDefinitionVersionTag === caseDefinitionVersionTag
+      ) {
+        return true;
+      }
+      return false;
+    })
+  );
+
   public readonly _caseDefinitionTitle$: Observable<string> = this.globalActiveCase$.pipe(
     map(result => result.name)
   );
