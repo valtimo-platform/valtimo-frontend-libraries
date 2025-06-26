4b6536e327840f06ac0bda9c4b8341d56dac837e
teodor-ritense <teodor.dragoi@ritense.com>
2025-06-26 12:20:15 +0200

    Fixes on FormManagement, ZGW, PluginManagement and CaseManagement

--- projects/valtimo/form-management/src/lib/components/form-management-duplicate/form-management-duplicate.component.ts
+++ projects/valtimo/form-management/src/lib/components/form-management-duplicate/form-management-duplicate.component.ts
@@ -64,8 +64,9 @@ export class FormManagementDuplicateComponent extends BaseModal implements OnIni
 
   constructor(
     @Inject('formToDuplicate') public readonly formToDuplicate: FormDefinition,
-    @Inject('disabledPendingChangesCallback')
-    public readonly disablePendingChangesCallback: () => void,
+    // TODO: Figure out why this is breaking
+    // @Inject('disabledPendingChangesCallback')
+    // public readonly disablePendingChangesCallback: () => void,
     @Inject('context') public readonly context: ManagementContext,
     @Inject('params') public readonly params: FormManagementParams,
     protected modalService: ModalService,
@@ -112,7 +113,7 @@ export class FormManagementDuplicateComponent extends BaseModal implements OnIni
       .pipe(take(1))
       .subscribe({
         next: formDefinition => {
-          this.disablePendingChangesCallback();
+          // this.disablePendingChangesCallback();
           this.navigateWithNewId(formDefinition.id).then(() => {
             this.closeModal();
             this.notificationService.showToast({
