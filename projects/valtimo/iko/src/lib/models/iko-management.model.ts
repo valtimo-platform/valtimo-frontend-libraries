00f2bee3003ca111b2f8562151a59f739173a008
Maxim Buur <maxim.buur@ritense.com>
2025-07-17 15:45:24 +0200

    add api service

---
+++ projects/valtimo/iko/src/lib/models/iko-management.model.ts
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+interface IkoDataAggregateCreateRequest {
+  ikoRepositoryConfigKey: string;
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoDataAggregateUpdateRequest {
+  ikoRepositoryConfigKey: string;
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoDataAggregateListResponse {
+  key: string;
+  title: string;
+}
+
+interface IkoDataAggregateResponse {
+  key: string;
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoDataRequestCreateRequest {
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoDataRequestUpdateRequest {
+  key: string;
+  ikoDataAggregateKey: string;
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoDataRequestListResponse {
+  key: string;
+  title: string;
+}
+
+interface IkoDataRequestResponse {
+  key: string;
+  ikoDataAggregateKey: string;
+  title: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoRepositoryConfigCreateRequest {
+  title: string;
+  type: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoRepositoryConfigUpdateRequest {
+  title: string;
+  type: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoRepositoryConfigListResponse {
+  key: string;
+  title: string;
+  type: string;
+}
+
+interface IkoRepositoryConfigResponse {
+  key: string;
+  title: string;
+  type: string;
+  properties: Record<string, any | null>;
+}
+
+interface IkoTabCreateRequest {
+  title: string;
+  type: string;
+}
+
+interface IkoTabUpdateRequest {
+  key: string;
+  title: string;
+  type: string;
+}
+
+interface TabDto {
+  key: string;
+  title?: string;
+  type: string;
+}
+
+interface PropertyField {
+  title: string;
+  key: string;
+  type: string;
+  dropdownList?: [string, string][];
+}
+
+interface WidgetAction {
+  key: string;
+  label: string;
+  type: string;
+  // Add more fields as necessary if known
+}
+
+interface WidgetDto {
+  key: string;
+  title: string;
+  width: number;
+  highContrast: boolean;
+  actions?: WidgetAction[];
+  type: string;
+}
+
+export {
+  IkoDataAggregateCreateRequest,
+  IkoDataAggregateUpdateRequest,
+  IkoDataAggregateListResponse,
+  IkoDataAggregateResponse,
+  IkoDataRequestCreateRequest,
+  IkoDataRequestUpdateRequest,
+  IkoDataRequestListResponse,
+  IkoDataRequestResponse,
+  IkoRepositoryConfigCreateRequest,
+  IkoRepositoryConfigUpdateRequest,
+  IkoRepositoryConfigListResponse,
+  IkoRepositoryConfigResponse,
+  IkoTabCreateRequest,
+  IkoTabUpdateRequest,
+  TabDto,
+  PropertyField,
+  WidgetDto,
+  WidgetAction,
+};
---
+++ projects/valtimo/iko/src/lib/services/iko-management-api.service.ts
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {BaseApiService, ConfigService} from '@valtimo/shared';
+import {Observable} from 'rxjs';
+import {
+  IkoDataAggregateCreateRequest,
+  IkoDataAggregateListResponse,
+  IkoDataAggregateResponse,
+  IkoDataAggregateUpdateRequest,
+  IkoDataRequestCreateRequest,
+  IkoDataRequestListResponse,
+  IkoDataRequestResponse,
+  IkoDataRequestUpdateRequest,
+  IkoRepositoryConfigCreateRequest,
+  IkoRepositoryConfigListResponse,
+  IkoRepositoryConfigResponse,
+  IkoRepositoryConfigUpdateRequest,
+  IkoTabCreateRequest,
+  IkoTabUpdateRequest,
+  PropertyField,
+  TabDto,
+  WidgetDto,
+} from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IkoManagementApiService extends BaseApiService {
+  constructor(
+    protected override httpClient: HttpClient,
+    protected override configService: ConfigService
+  ) {
+    super(httpClient, configService);
+  }
+
+  public getIkoDataAggregates(
+    key?: string,
+    title?: string,
+    page: number = 0,
+    size: number = 100,
+    sort: string = 'title,asc'
+  ): Observable<{content: IkoDataAggregateListResponse[]}> {
+    let params = new HttpParams().set('page', page).set('size', size).set('sort', sort);
+    if (key) params = params.set('key', key);
+    if (title) params = params.set('title', title);
+    return this.httpClient.get<{content: IkoDataAggregateListResponse[]}>(
+      this.getApiUrl('/v1/iko-data-aggregate'),
+      {params}
+    );
+  }
+
+  public getIkoDataAggregate(key: string): Observable<IkoDataAggregateResponse> {
+    return this.httpClient.get<IkoDataAggregateResponse>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${key}`)
+    );
+  }
+
+  public createIkoDataAggregate(
+    key: string,
+    body: IkoDataAggregateCreateRequest
+  ): Observable<IkoDataAggregateResponse> {
+    return this.httpClient.post<IkoDataAggregateResponse>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${key}`),
+      body
+    );
+  }
+
+  public updateIkoDataAggregate(
+    key: string,
+    body: IkoDataAggregateUpdateRequest
+  ): Observable<IkoDataAggregateResponse> {
+    return this.httpClient.put<IkoDataAggregateResponse>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${key}`),
+      body
+    );
+  }
+
+  public deleteIkoDataAggregate(key: string): Observable<void> {
+    return this.httpClient.delete<void>(this.getApiUrl(`/v1/iko-data-aggregate/${key}`));
+  }
+
+  public getIkoDataAggregatePropertyFields(type: string): Observable<PropertyField[]> {
+    return this.httpClient.get<PropertyField[]>(
+      this.getApiUrl(`/v1/iko-property-fields/${type}/data-aggregate`)
+    );
+  }
+
+  public getIkoDataRequests(aggregateKey: string): Observable<IkoDataRequestListResponse[]> {
+    return this.httpClient.get<IkoDataRequestListResponse[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/data-request`)
+    );
+  }
+
+  public getIkoDataRequest(aggregateKey: string, key: string): Observable<IkoDataRequestResponse> {
+    return this.httpClient.get<IkoDataRequestResponse>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/data-request/${key}`)
+    );
+  }
+
+  public createIkoDataRequest(
+    aggregateKey: string,
+    key: string,
+    body: IkoDataRequestCreateRequest
+  ): Observable<IkoDataRequestResponse> {
+    return this.httpClient.post<IkoDataRequestResponse>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/data-request/${key}`),
+      body
+    );
+  }
+
+  public updateIkoDataRequests(
+    aggregateKey: string,
+    body: IkoDataRequestUpdateRequest[]
+  ): Observable<IkoDataRequestResponse[]> {
+    return this.httpClient.put<IkoDataRequestResponse[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/data-request`),
+      body
+    );
+  }
+
+  public deleteIkoDataRequest(aggregateKey: string, key: string): Observable<void> {
+    return this.httpClient.delete<void>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/data-request/${key}`)
+    );
+  }
+
+  public getIkoDataRequestPropertyFields(type: string): Observable<PropertyField[]> {
+    return this.httpClient.get<PropertyField[]>(
+      this.getApiUrl(`/v1/iko-property-fields/${type}/data-request`)
+    );
+  }
+
+  public getIkoRepositoryConfigs(
+    title?: string,
+    type?: string,
+    page: number = 0,
+    size: number = 100,
+    sort: string = 'title,asc'
+  ): Observable<{content: IkoRepositoryConfigListResponse[]}> {
+    let params = new HttpParams().set('page', page).set('size', size).set('sort', sort);
+    if (title) params = params.set('title', title);
+    if (type) params = params.set('type', type);
+    return this.httpClient.get<{content: IkoRepositoryConfigListResponse[]}>(
+      this.getApiUrl(`/v1/iko`),
+      {params}
+    );
+  }
+
+  public getIkoRepositoryConfig(key: string): Observable<IkoRepositoryConfigResponse> {
+    return this.httpClient.get<IkoRepositoryConfigResponse>(this.getApiUrl(`/v1/iko/${key}`));
+  }
+
+  public createIkoRepositoryConfig(
+    key: string,
+    body: IkoRepositoryConfigCreateRequest
+  ): Observable<IkoRepositoryConfigResponse> {
+    return this.httpClient.post<IkoRepositoryConfigResponse>(
+      this.getApiUrl(`/v1/iko/${key}`),
+      body
+    );
+  }
+
+  public updateIkoRepositoryConfig(
+    key: string,
+    body: IkoRepositoryConfigUpdateRequest
+  ): Observable<IkoRepositoryConfigResponse> {
+    return this.httpClient.put<IkoRepositoryConfigResponse>(this.getApiUrl(`/v1/iko/${key}`), body);
+  }
+
+  public deleteIkoRepositoryConfig(key: string): Observable<void> {
+    return this.httpClient.delete<void>(this.getApiUrl(`/v1/iko/${key}`));
+  }
+
+  public getIkoRepositoryConfigPropertyFields(type: string): Observable<PropertyField[]> {
+    return this.httpClient.get<PropertyField[]>(
+      this.getApiUrl(`/v1/iko-property-fields/${type}/repository-config`)
+    );
+  }
+
+  public getIkoRepositoryTypes(): Observable<string[]> {
+    return this.httpClient.get<string[]>(this.getApiUrl(`/v1/iko-types`));
+  }
+
+  public getIkoTabs(aggregateKey: string): Observable<TabDto[]> {
+    return this.httpClient.get<TabDto[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab`)
+    );
+  }
+
+  public getIkoTab(aggregateKey: string, tabKey: string): Observable<TabDto> {
+    return this.httpClient.get<TabDto>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}`)
+    );
+  }
+
+  public createIkoTab(
+    aggregateKey: string,
+    tabKey: string,
+    body: IkoTabCreateRequest
+  ): Observable<TabDto> {
+    return this.httpClient.post<TabDto>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}`),
+      body
+    );
+  }
+
+  public updateIkoTabs(aggregateKey: string, body: IkoTabUpdateRequest[]): Observable<TabDto[]> {
+    return this.httpClient.put<TabDto[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab`),
+      body
+    );
+  }
+
+  public deleteIkoTab(aggregateKey: string, tabKey: string): Observable<void> {
+    return this.httpClient.delete<void>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}`)
+    );
+  }
+
+  public getIkoWidgets(aggregateKey: string, tabKey: string): Observable<WidgetDto[]> {
+    return this.httpClient.get<WidgetDto[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}/widget`)
+    );
+  }
+
+  public getIkoWidget(
+    aggregateKey: string,
+    tabKey: string,
+    widgetKey: string
+  ): Observable<WidgetDto> {
+    return this.httpClient.get<WidgetDto>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}/widget/${widgetKey}`)
+    );
+  }
+
+  public createIkoWidget(
+    aggregateKey: string,
+    tabKey: string,
+    widgetKey: string,
+    body: WidgetDto
+  ): Observable<WidgetDto> {
+    return this.httpClient.post<WidgetDto>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}/widget/${widgetKey}`),
+      body
+    );
+  }
+
+  public updateIkoWidgets(
+    aggregateKey: string,
+    tabKey: string,
+    body: WidgetDto[]
+  ): Observable<WidgetDto[]> {
+    return this.httpClient.put<WidgetDto[]>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}/widget`),
+      body
+    );
+  }
+
+  public deleteIkoWidget(
+    aggregateKey: string,
+    tabKey: string,
+    widgetKey: string
+  ): Observable<void> {
+    return this.httpClient.delete<void>(
+      this.getApiUrl(`/v1/iko-data-aggregate/${aggregateKey}/tab/${tabKey}/widget/${widgetKey}`)
+    );
+  }
+}
