c8ef98eff1c2d4c71f31c7e9bf48151462f22f8f
Maxim Buur <maxim.buur@ritense.com>
2025-07-18 12:42:24 +0200

    work on tabs

---
+++ projects/valtimo/iko/src/lib/components/iko-management-details/components/search-fields/iko-management-search-fields.component.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {CarbonListModule} from '@valtimo/components';
+import {TabsModule} from 'carbon-components-angular';
+
+@Component({
+  standalone: true,
+  templateUrl: './iko-management-search-fields.component.html',
+  imports: [CommonModule, CarbonListModule, TabsModule],
+})
+export class IkoManagementSearchFieldsComponent {}
