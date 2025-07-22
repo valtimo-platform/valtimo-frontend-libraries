feb21b50c889d4d78411d7c2760ffd653cdfb093
Maxim Buur <maxim.buur@ritense.com>
2025-07-22 14:23:35 +0200

    wip modal

--- projects/valtimo/iko/src/lib/components/iko-management-api/iko-management-api.component.ts
+++ projects/valtimo/iko/src/lib/components/iko-management-api/iko-management-api.component.ts
@@ -15,24 +15,59 @@
  */
 import {CommonModule} from '@angular/common';
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {CarbonListModule, ColumnConfig, PageTitleService} from '@valtimo/components';
+import {
+  CARBON_CONSTANTS,
+  CarbonListModule,
+  ColumnConfig,
+  PageTitleService,
+  ValtimoCdsModalDirective,
+} from '@valtimo/components';
 import {IkoApiService, IkoManagementApiService} from '../../services';
 import {BehaviorSubject, combineLatest, filter, Subscription, tap} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
 import {map} from 'rxjs/operators';
-import {TabsModule} from 'carbon-components-angular';
+import {
+  ButtonModule,
+  IconModule,
+  InputModule,
+  LayerModule,
+  ModalModule,
+  TabsModule,
+} from 'carbon-components-angular';
 import {TranslateModule} from '@ngx-translate/core';
 import {IKO_MANAGEMENT_TABS} from '../../constants';
 import {IkoRepositoryConfigListResponse} from '../../models';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'valtimo-iko-management-details',
+  selector: 'valtimo-iko-management-api',
   standalone: true,
   templateUrl: './iko-management-api.component.html',
-  imports: [CommonModule, CarbonListModule, TabsModule, TranslateModule],
+  imports: [
+    CommonModule,
+    CarbonListModule,
+    TabsModule,
+    TranslateModule,
+    ModalModule,
+    ButtonModule,
+    IconModule,
+    FormsModule,
+    InputModule,
+    ReactiveFormsModule,
+    ValtimoCdsModalDirective,
+    LayerModule,
+  ],
   styleUrl: './iko-management-api.component.scss',
 })
 export class IkoManagementApiComponent implements OnInit, OnDestroy {
+  public readonly openModal$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   public readonly loading$ = new BehaviorSubject<boolean>(true);
 
   private readonly _key$ = this.route.params.pipe(
@@ -65,6 +100,12 @@ export class IkoManagementApiComponent implements OnInit, OnDestroy {
 
   public readonly TABS = IKO_MANAGEMENT_TABS;
 
+  public readonly form = this.formBuilder.group({
+    pluginId: this.formBuilder.control('', [Validators.required]),
+    apiKey: this.formBuilder.control('', [Validators.required]),
+    baseUrl: this.formBuilder.control('', [Validators.required]),
+  });
+
   private readonly _subscriptions = new Subscription();
 
   constructor(
@@ -72,7 +113,8 @@ export class IkoManagementApiComponent implements OnInit, OnDestroy {
     private readonly ikoApiService: IkoApiService,
     private readonly route: ActivatedRoute,
     private readonly pageTitleService: PageTitleService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
   ) {}
 
   public ngOnInit(): void {
@@ -86,5 +128,43 @@ export class IkoManagementApiComponent implements OnInit, OnDestroy {
 
   public onRowClicked(event: IkoRepositoryConfigListResponse): void {
     this.router.navigate(['iko-management', event.key]);
+  }
+
+  public openModal(): void {
+    this.openModal$.next(true);
+  }
+
+  public closeModal(): void {
+    this.openModal$.next(false);
+
+    setTimeout(() => {
+      this.form.reset();
+    }, CARBON_CONSTANTS.modalAnimationMs);
+  }
+
+  public getControlInvalid(controlKey: string): boolean {
+    const control: AbstractControl | null = this.form.get(controlKey);
+
+    if (!control) {
+      return true;
+    }
+
+    return !control.valid && !control.pristine;
+  }
+
+  public createApiConfig(): void {
+    // const control: AbstractControl | null = this.form.get('title');
+    // if (!control || !control.valid) {
+    //   return;
+    // }
+    //
+    // const newDashboard: DashboardItem = this.form.getRawValue();
+    // this.dashboardManagementService.dispatchAction(
+    //   this.dashboardManagementService.createDashboard(newDashboard).pipe(
+    //     finalize(() => {
+    //       this.closeModal();
+    //     })
+    //   )
+    // );
   }
 }
