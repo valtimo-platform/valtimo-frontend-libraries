valtimo-platform/valtimo

prefill informatieobject type based on name. (#1577)

frontend/projects/valtimo/zgw/src/lib/modules/documenten-api/formio/documenten-api-uploader/documenten-api-uploader.component.ts
@@ -15,21 +15,29 @@
  */
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, signal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {FormioCustomComponent, FormIoDomService, ValtimoModalService} from '@valtimo/components';
+import {
+  FormioCustomComponent,
+  FormIoDomService,
+  ValtimoModalService,
+  FormIoStateService,
+} from '@valtimo/components';
 import {DocumentenApiFileReference, UploadProviderService} from '@valtimo/resource';
 import {UserProviderService} from '@valtimo/security';
 import {
   BehaviorSubject,
   combineLatest,
+  EMPTY,
   Observable,
   of,
   startWith,
   Subscription,
   switchMap,
 } from 'rxjs';
-import {filter, map, take, tap} from 'rxjs/operators';
+import {catchError, filter, map, take, tap} from 'rxjs/operators';
 import {DocumentenApiMetadata, SupportedDocumentenApiFeatures} from '../../models';
 import {DocumentenApiVersionService} from '../../services';
+import {DocumentService} from '@valtimo/document';
+import {getCaseManagementRouteParams} from '@valtimo/shared';
 
 @Component({
   standalone: false,
@@ -100,6 +108,30 @@ export class DocumentenApiUploaderComponent
 
   @Input() set documentType(defaultValue: string) {
     this.defaultValues['informatieobjecttype'] = defaultValue;
+    this.stateService.documentDefinitionName$
+      .pipe(
+        switchMap(documentDefinitionName =>
+          this.documentService.getCaseSettings(documentDefinitionName)
+        ),
+        switchMap(caseDefinition =>
+          this.documentService.getDocumentTypesForCase(
+            String(caseDefinition.caseDefinitionKey),
+            String(caseDefinition.caseDefinitionVersionTag)
+          )
+        ),
+        catchError(() => {
+          this.defaultValues['informatieobjecttype'] = defaultValue;
+          return EMPTY;
+        })
+      )
+      .subscribe(documentTypes => {
+        const foundDocumentType = documentTypes.find(
+          documentType => documentType.name === defaultValue
+        );
+        foundDocumentType
+          ? (this.defaultValues['informatieobjecttype'] = foundDocumentType.url)
+          : (this.defaultValues['informatieobjecttype'] = defaultValue);
+      });
   }
 
   @Input() set hideDocumentType(hide: boolean) {
@@ -188,7 +220,9 @@ export class DocumentenApiUploaderComponent
     private readonly modalService: ValtimoModalService,
     private readonly userProviderService: UserProviderService,
     private readonly route: ActivatedRoute,
-    private readonly documentenApiVersionService: DocumentenApiVersionService
+    private readonly documentenApiVersionService: DocumentenApiVersionService,
+    private readonly stateService: FormIoStateService,
+    private readonly documentService: DocumentService
   ) {}
 
   public ngOnInit(): void {

valtimo-platform/valtimo-frontend-libraries

projects/valtimo/zgw/src/lib/modules/documenten-api/formio/documenten-api-uploader/documenten-api-uploader.component.ts
@@ -13,32 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, signal} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {
   FormioCustomComponent,
   FormIoDomService,
   FormIoStateService,
   ValtimoModalService,
 } from '@valtimo/components';
+import {DocumentService} from '@valtimo/document';
+import {DocumentenApiFileReference, UploadProviderService} from '@valtimo/resource';
+import {UserProviderService} from '@valtimo/security';
 import {
   BehaviorSubject,
   combineLatest,
+  EMPTY,
   Observable,
   of,
   startWith,
   Subscription,
   switchMap,
 } from 'rxjs';
-import {
-  DocumentenApiFileReference,
-  DownloadService,
-  UploadProviderService,
-} from '@valtimo/resource';
+import {catchError, filter, map, take, tap} from 'rxjs/operators';
 import {DocumentenApiMetadata, SupportedDocumentenApiFeatures} from '../../models';
-import {filter, map, take, tap} from 'rxjs/operators';
-import {UserProviderService} from '@valtimo/security';
-import {ActivatedRoute} from '@angular/router';
 import {DocumentenApiVersionService} from '../../services';
 
 @Component({
@@ -109,6 +106,24 @@ export class DocumentenApiUploaderComponent
 
   @Input() set documentType(defaultValue: string) {
     this.defaultValues['informatieobjecttype'] = defaultValue;
+    this.stateService.documentDefinitionName$
+      .pipe(
+        switchMap(documentDefinitionName =>
+          this.documentService.getDocumentTypes(documentDefinitionName)
+        ),
+        catchError(() => {
+          this.defaultValues['informatieobjecttype'] = defaultValue;
+          return EMPTY;
+        })
+      )
+      .subscribe(documentTypes => {
+        const foundDocumentType = documentTypes.find(
+          documentType => documentType.name === defaultValue
+        );
+        foundDocumentType
+          ? (this.defaultValues['informatieobjecttype'] = foundDocumentType.url)
+          : (this.defaultValues['informatieobjecttype'] = defaultValue);
+      });
   }
 
   @Input() set hideDocumentType(hide: boolean) {
@@ -193,13 +208,13 @@ export class DocumentenApiUploaderComponent
 
   constructor(
     private readonly uploadProviderService: UploadProviderService,
-    private readonly stateService: FormIoStateService,
     private readonly domService: FormIoDomService,
-    private readonly downloadService: DownloadService,
     private readonly modalService: ValtimoModalService,
     private readonly userProviderService: UserProviderService,
     private readonly route: ActivatedRoute,
-    private readonly documentenApiVersionService: DocumentenApiVersionService
+    private readonly documentenApiVersionService: DocumentenApiVersionService,
+    private readonly stateService: FormIoStateService,
+    private readonly documentService: DocumentService
   ) {}
 
   public ngOnInit(): void {