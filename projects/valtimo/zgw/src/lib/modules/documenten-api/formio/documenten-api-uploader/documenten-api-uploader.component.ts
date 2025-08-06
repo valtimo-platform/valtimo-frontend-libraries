5b0a5359327834213899a65432b8246e947692b8
Sofia <131170700+sofiaIvarsRitense@users.noreply.github.com>
2025-08-06 11:31:09 +0200

    Fix to enable the submit button correctly on a Form.io form inside a user task (#1591)

--- projects/valtimo/zgw/src/lib/modules/documenten-api/formio/documenten-api-uploader/documenten-api-uploader.component.ts
+++ projects/valtimo/zgw/src/lib/modules/documenten-api/formio/documenten-api-uploader/documenten-api-uploader.component.ts
@@ -187,18 +187,18 @@ export class DocumentenApiUploaderComponent
 
   @Output() valueChange = new EventEmitter<Array<DocumentenApiFileReference>>();
 
-  readonly uploading$ = new BehaviorSubject<boolean>(false);
-  readonly fileToBeUploaded$ = new BehaviorSubject<File | null>(null);
-  readonly modalDisabled$ = new BehaviorSubject<boolean>(false);
-  readonly showModal = signal<boolean>(false);
-  readonly uploadProcessLinked$: Observable<boolean | string> =
+  public readonly uploading$ = new BehaviorSubject<boolean>(false);
+  public readonly fileToBeUploaded$ = new BehaviorSubject<File | null>(null);
+  public readonly modalDisabled$ = new BehaviorSubject<boolean>(false);
+  public readonly showModal = signal<boolean>(false);
+  public readonly uploadProcessLinked$: Observable<boolean | string> =
     this.modalService.caseDefinitionKey$.pipe(
       switchMap(caseDefinitionKey =>
         this.uploadProviderService.checkUploadProcessLink(caseDefinitionKey)
       ),
       startWith('loading')
     );
-  readonly isAdmin$: Observable<boolean> = this.userProviderService
+  public readonly isAdmin$: Observable<boolean> = this.userProviderService
     .getUserSubject()
     .pipe(map(userIdentity => userIdentity?.roles.includes('ROLE_ADMIN')));
 
@@ -246,12 +246,12 @@ export class DocumentenApiUploaderComponent
     }
   }
 
-  fileSelected(file: File): void {
+  public fileSelected(file: File): void {
     this.fileToBeUploaded$.next(file);
     this.showModal.set(true);
   }
 
-  deleteFile(id: string): void {
+  public deleteFile(id: string): void {
     this.domService.toggleSubmitButton(true);
     this._value = this._value.filter((file: DocumentenApiFileReference) =>
       file?.id ? file?.id !== id : true
@@ -259,11 +259,11 @@ export class DocumentenApiUploaderComponent
     this.valueChange.emit(this._value);
   }
 
-  closeMetadataModal(): void {
+  public closeMetadataModal(): void {
     this.showModal.set(false);
   }
 
-  metadataSet(metadata: DocumentenApiMetadata): void {
+  public metadataSet(metadata: DocumentenApiMetadata): void {
     this.uploading$.next(true);
     this.showModal.set(false);
     this.domService.toggleSubmitButton(true);
@@ -275,7 +275,7 @@ export class DocumentenApiUploaderComponent
         tap(result => {
           this.domService.toggleSubmitButton(false);
           this.uploading$.next(false);
-          this._value.push(result);
+          this._value = [...this._value, result];
           this.valueChange.emit(this._value);
         })
       )
